0578f4cb6a2dbbb6cedfc81d8f010e40543e661b
MohdSuhail <mohd.suhail@tecxpert.in>
2023-06-15 16:08:10 +0530

    added manual addition  and updated crystal report

--- XpertERP/Purchase/Report/rptMultipleDeductionReport.vb
+++ XpertERP/Purchase/Report/rptMultipleDeductionReport.vb
@@ -260,8 +260,8 @@ where TSPL_MULTIPLE_DEDUCTION_HEAD.IsPosted=1 and convert(date,TSPL_MULTIPLE_DED
     End Sub
 
     Private Sub txtLocation__My_Click(sender As Object, e As EventArgs) Handles txtLocation._My_Click
-        ' Dim qry As String = " select Location_Code as Code, Location_Desc as Name from TSPL_LOCATION_MASTER where  Loc_Status='N' and Location_Type='Physical' and Is_Section='N' and Is_Sub_Location='N' and CSA_Type <>'Y' and DutyPaid <>'Y' and Rejected_Type <>'Y' and GIT_Type<>'Y'"
-        Dim qry As String = "select MCC_Code as Code ,MCC_NAME  as Name from TSPL_MCC_MASTER"
+        Dim qry As String = " select Location_Code as Code, Location_Desc as Name from TSPL_LOCATION_MASTER where  Loc_Status='N' and Location_Type='Physical' and Is_Section='N' and Is_Sub_Location='N' and CSA_Type <>'Y' and DutyPaid <>'Y' and Rejected_Type <>'Y' and GIT_Type<>'Y'"
+        'Dim qry As String = "select MCC_Code as Code ,MCC_NAME  as Name from TSPL_MCC_MASTER"
         txtLocation.arrValueMember = clsCommon.ShowMultipleSelectForm("TransDetailedCardReport", qry, "Code", "Name", txtLocation.arrValueMember, txtLocation.arrDispalyMember)
     End Sub
     Private Sub rmsaveLayout_Click(sender As Object, e As EventArgs) Handles rmsaveLayout.Click
