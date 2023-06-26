f46c1659e435853a6aabb03e8bd17a2a510a8715
Gaurav kumar Singh <tecxpert07@outlook.com>
2023-06-26 10:36:42 +0530

    frmMPDCSIncentiveReco Customized Import option added in it.

---
+++ XpertERPEngine/WinForm/Excelform.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Excelform
+    'Inherits System.Windows.Forms.Form
+    Inherits FrmMainTranScreen
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim TableViewDefinition3 As Telerik.WinControls.UI.TableViewDefinition = New Telerik.WinControls.UI.TableViewDefinition()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.gvExcel = New common.UserControls.MyRadGridView()
+        Me.btnCancel = New Telerik.WinControls.UI.RadButton()
+        Me.btnOK = New Telerik.WinControls.UI.RadButton()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.gvExcel, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gvExcel.MasterTemplate, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnCancel, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnOK, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.gvExcel)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnCancel)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnOK)
+        Me.SplitContainer1.Size = New System.Drawing.Size(579, 402)
+        Me.SplitContainer1.SplitterDistance = 370
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'gvExcel
+        '
+        Me.gvExcel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.gvExcel.Location = New System.Drawing.Point(0, 0)
+        '
+        '
+        '
+        Me.gvExcel.MasterTemplate.AllowAddNewRow = False
+        Me.gvExcel.MasterTemplate.SelectionMode = Telerik.WinControls.UI.GridViewSelectionMode.CellSelect
+        Me.gvExcel.MasterTemplate.ShowHeaderCellButtons = True
+        Me.gvExcel.MasterTemplate.ViewDefinition = TableViewDefinition3
+        Me.gvExcel.Name = "gvExcel"
+        Me.gvExcel.ShowHeaderCellButtons = True
+        Me.gvExcel.Size = New System.Drawing.Size(579, 370)
+        Me.gvExcel.TabIndex = 1
+        Me.gvExcel.TabStop = False
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnCancel.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(269, 3)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(77, 21)
+        Me.btnCancel.TabIndex = 16
+        Me.btnCancel.Text = "Esc : Cancel"
+        '
+        'btnOK
+        '
+        Me.btnOK.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnOK.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOK.Location = New System.Drawing.Point(182, 3)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(77, 21)
+        Me.btnOK.TabIndex = 15
+        Me.btnOK.Text = "F5 : OK"
+        '
+        'Excelform
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(579, 402)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Excelform"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SetCustomizedExcel"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.ResumeLayout(False)
+        CType(Me.gvExcel.MasterTemplate, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gvExcel, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnCancel, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnOK, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents btnOK As RadButton
+    Friend WithEvents btnCancel As RadButton
+    Friend WithEvents gvExcel As UserControls.MyRadGridView
+End Class
---
+++ XpertERPEngine/WinForm/Excelform.vb
@@ -0,0 +1,163 @@
+﻿Imports common
+Imports System.Windows.Forms
+
+Public Class Excelform
+    Inherits FrmMainTranScreen
+
+    Public comboDataSource As DataTable
+    Public collReturn As Dictionary(Of String, String)
+    Const colSno As String = "colSNO"
+    Const colValue As String = "colValue"
+    Const colMatching As String = "colMatching"
+    Private Sub Cancel()
+        Me.Close()
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Cancel()
+    End Sub
+
+    Private Sub OkPressed()
+        Try
+            ''Check all value in matching column selected
+
+            Dim matchingValues As New HashSet(Of String)()
+            For ii As Integer = 0 To gvExcel.Rows.Count - 1
+                Dim value As String = clsCommon.myCstr(gvExcel.Rows(ii).Cells(colMatching).Value)
+                If String.IsNullOrEmpty(value) Then
+                    Throw New Exception("Value is not filled in every row.")
+                End If
+
+                If matchingValues.Contains(value) Then
+                    Throw New Exception("Duplicate Value Found.")
+                End If
+
+                matchingValues.Add(value)
+            Next
+
+            ''No Repeat
+
+            collReturn = New Dictionary(Of String, String)
+            For ii As Integer = 0 To gvExcel.Rows.Count - 1
+                collReturn.Add(clsCommon.myCstr(gvExcel.Rows(ii).Cells(colValue).Value), clsCommon.myCstr(gvExcel.Rows(ii).Cells(colMatching).Value))
+            Next
+            Me.Close()
+        Catch ex As Exception
+            clsCommon.MyMessageBoxShow(Me, ex.Message, Me.Text)
+        End Try
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        OkPressed()
+    End Sub
+
+    Sub ExcelGrid()
+        gvExcel.Rows.Clear()
+        gvExcel.Columns.Clear()
+        gvExcel.DataSource = Nothing
+
+        Dim lineNo As New GridViewTextBoxColumn()
+        lineNo.FormatString = ""
+        lineNo.HeaderText = "SNo"
+        lineNo.Name = colSno
+        lineNo.Width = 60
+        lineNo.ReadOnly = True
+        lineNo.TextAlignment = System.Drawing.ContentAlignment.MiddleRight
+        gvExcel.Columns.Add(lineNo)
+
+
+        Dim farmercode As New GridViewTextBoxColumn()
+        farmercode.FormatString = ""
+        farmercode.HeaderText = "Value Column"
+        farmercode.Name = colValue
+        farmercode.Width = 100
+        farmercode.ReadOnly = True
+        farmercode.IsVisible = True
+        farmercode.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft
+        gvExcel.Columns.Add(farmercode)
+
+
+        Dim repoComboBox As GridViewComboBoxColumn = New GridViewComboBoxColumn()
+        repoComboBox.FormatString = ""
+        repoComboBox.HeaderText = "Matching Column"
+        repoComboBox.Name = colMatching
+        repoComboBox.Width = 150
+        repoComboBox.ReadOnly = False
+        repoComboBox.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft
+        repoComboBox.DataSource = comboDataSource
+        repoComboBox.ValueMember = "Code"
+        repoComboBox.DisplayMember = "Code"
+        repoComboBox.IsVisible = True
+        gvExcel.MasterTemplate.Columns.Add(repoComboBox)
+
+
+        gvExcel.AllowAddNewRow = False
+        gvExcel.AllowDeleteRow = False
+        gvExcel.AllowRowReorder = False
+        gvExcel.ShowGroupPanel = False
+        gvExcel.EnableFiltering = False
+        gvExcel.ShowFilteringRow = True
+        gvExcel.EnableSorting = False
+        gvExcel.EnableGrouping = False
+        gvExcel.AddNewRowPosition = Telerik.WinControls.UI.SystemRowPosition.Bottom
+
+        ''Create all five row
+
+        gvExcel.Rows.AddNew()
+        gvExcel.Rows(0).Cells(colSno).Value = 1
+        gvExcel.Rows(0).Cells(colValue).Value = "DCS Code"
+
+        gvExcel.Rows.AddNew()
+        gvExcel.Rows(1).Cells(colSno).Value = 2
+        gvExcel.Rows(1).Cells(colValue).Value = "Year"
+
+        gvExcel.Rows.AddNew()
+        gvExcel.Rows(2).Cells(colSno).Value = 3
+        gvExcel.Rows(2).Cells(colValue).Value = "Month"
+
+        gvExcel.Rows.AddNew()
+        gvExcel.Rows(3).Cells(colSno).Value = 4
+        gvExcel.Rows(3).Cells(colValue).Value = "Cycle No"
+
+        gvExcel.Rows.AddNew()
+        gvExcel.Rows(4).Cells(colSno).Value = 5
+        gvExcel.Rows(4).Cells(colValue).Value = "Qty"
+
+
+    End Sub
+
+    Private Sub Excelform_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ExcelGrid()
+    End Sub
+
+    'Private Sub importExcel()
+    '    'UcAttachment1.BlankAllControls()
+    '    Dim dgv As New RadGridView
+    '    Me.Controls.Add(dgv)
+    '    Dim ii As Integer = 0
+
+    '    'loadBlankGrid()
+    '    Dim FileName As String = ""
+    '    Dim SafeFileName As String = ""
+    '    If transportSql.importExcel(FileName, SafeFileName, dgv, "DCS Code", "Year", "Month", "Cycle No", "Qty") Then
+    '        'UcAttachment1.AddAttachment(FileName, SafeFileName)
+
+    '        gvExcel.Columns.Add("DCS Code")
+    '        gvExcel.Columns.Add("Year")
+    '        gvExcel.Columns.Add("Month")
+    '        gvExcel.Columns.Add("Cycle No")
+    '        gvExcel.Columns.Add("Qty")
+
+
+
+
+    '    End If
+
+    '    common.clsCommon.MyMessageBoxShow("Imported Successfully", Me.Text)
+    'End Sub
+
+    'Private Sub btn_Import_Click(sender As Object, e As EventArgs)
+    '    importExcel()
+    'End Sub
+
+End Class
