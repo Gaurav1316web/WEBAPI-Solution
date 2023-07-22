41adaf5c00fb056c910c3f4d48c94a6770fdc6b7
MohdSuhail <mohd.suhail@tecxpert.in>
2023-07-22 10:25:19 +0530

    resolved current outstanding pdf/excel

--- XpertERPEngine/Milk Procurement/clsMilkSampleMCC.vb
+++ XpertERPEngine/Milk Procurement/clsMilkSampleMCC.vb
@@ -1498,9 +1498,8 @@ Public Class clsMilkSRNMCC
                 clsDBFuncationality.ExecuteNonQuery(qry, Trans)
                 qry = "update TSPL_MILK_SRN_HEAD set VLC_CODE='" + objHead.VLC_CODE + "',VSP_CODE='" + objHead.VSP_CODE + "',ROUTE_CODE='" + objHead.ROUTE_CODE + "'  where DOC_CODE='" + objHead.DOC_CODE + "'"
                 clsDBFuncationality.ExecuteNonQuery(qry, Trans)
-               '
-                qry = "update TSPL_MILK_SRN_HEAD set Retesting = 1 where DOC_CODE='" + objHead.DOC_CODE + "'"
-                clsDBFuncationality.ExecuteNonQuery(qry, Trans)
+                '
+
                 '
             End If
             If CorrTypeSRNFATSNF Then
