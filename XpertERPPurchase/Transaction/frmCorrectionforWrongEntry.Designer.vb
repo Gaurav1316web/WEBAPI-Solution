00ca70c3ba4d5b48ee10a786179b2c3e7f59fc29
Gaurav kumar Singh <tecxpert07@outlook.com>
2023-08-23 10:48:37 +0530

    New Screen Correctionforwrongentry.

---
+++ XpertERPPurchase/Transaction/frmCorrectionforWrongEntry.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmCorrectionforWrongEntry
+    'Inherits System.Windows.Forms.Form
+    Inherits FrmMainTranScreen
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.RadPageView1 = New Telerik.WinControls.UI.RadPageView()
+        Me.RadPageViewPage1 = New Telerik.WinControls.UI.RadPageViewPage()
+        Me.MyLabel1 = New common.Controls.MyLabel()
+        Me.txtLRNo = New common.Controls.MyTextBox()
+        Me.txtGRNo = New common.Controls.MyTextBox()
+        Me.RadLabel13 = New common.Controls.MyLabel()
+        Me.txtVehicleNo = New common.Controls.MyTextBox()
+        Me.RadLabel5 = New common.Controls.MyLabel()
+        Me.RadLabel1 = New common.Controls.MyLabel()
+        Me.txtDocNo = New common.UserControls.txtNavigator()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.RadPageView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.RadPageView1.SuspendLayout()
+        Me.RadPageViewPage1.SuspendLayout()
+        CType(Me.MyLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtLRNo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtGRNo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtVehicleNo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.RadPageView1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(800, 450)
+        Me.SplitContainer1.SplitterDistance = 412
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'RadPageView1
+        '
+        Me.RadPageView1.Controls.Add(Me.RadPageViewPage1)
+        Me.RadPageView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RadPageView1.Location = New System.Drawing.Point(0, 0)
+        Me.RadPageView1.Name = "RadPageView1"
+        Me.RadPageView1.SelectedPage = Me.RadPageViewPage1
+        Me.RadPageView1.Size = New System.Drawing.Size(800, 412)
+        Me.RadPageView1.TabIndex = 0
+        CType(Me.RadPageView1.GetChildAt(0), Telerik.WinControls.UI.RadPageViewStripElement).StripButtons = Telerik.WinControls.UI.StripViewButtons.None
+        '
+        'RadPageViewPage1
+        '
+        Me.RadPageViewPage1.Controls.Add(Me.MyLabel1)
+        Me.RadPageViewPage1.Controls.Add(Me.txtLRNo)
+        Me.RadPageViewPage1.Controls.Add(Me.txtGRNo)
+        Me.RadPageViewPage1.Controls.Add(Me.RadLabel13)
+        Me.RadPageViewPage1.Controls.Add(Me.txtVehicleNo)
+        Me.RadPageViewPage1.Controls.Add(Me.RadLabel5)
+        Me.RadPageViewPage1.Controls.Add(Me.RadLabel1)
+        Me.RadPageViewPage1.Controls.Add(Me.txtDocNo)
+        Me.RadPageViewPage1.ItemSize = New System.Drawing.SizeF(115.0!, 28.0!)
+        Me.RadPageViewPage1.Location = New System.Drawing.Point(10, 37)
+        Me.RadPageViewPage1.Name = "RadPageViewPage1"
+        Me.RadPageViewPage1.Size = New System.Drawing.Size(779, 364)
+        Me.RadPageViewPage1.Text = "Gate Received Note"
+        '
+        'MyLabel1
+        '
+        Me.MyLabel1.FieldName = Nothing
+        Me.MyLabel1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MyLabel1.Location = New System.Drawing.Point(13, 74)
+        Me.MyLabel1.Name = "MyLabel1"
+        Me.MyLabel1.Size = New System.Drawing.Size(38, 16)
+        Me.MyLabel1.TabIndex = 47
+        Me.MyLabel1.Text = "LR No"
+        '
+        'txtLRNo
+        '
+        Me.txtLRNo.CalculationExpression = Nothing
+        Me.txtLRNo.FieldCode = Nothing
+        Me.txtLRNo.FieldDesc = Nothing
+        Me.txtLRNo.FieldMaxLength = 0
+        Me.txtLRNo.FieldName = Nothing
+        Me.txtLRNo.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLRNo.isCalculatedField = False
+        Me.txtLRNo.IsSourceFromTable = False
+        Me.txtLRNo.IsSourceFromValueList = False
+        Me.txtLRNo.IsUnique = False
+        Me.txtLRNo.Location = New System.Drawing.Point(106, 74)
+        Me.txtLRNo.MaxLength = 12
+        Me.txtLRNo.MendatroryField = False
+        Me.txtLRNo.MyLinkLable1 = Me.MyLabel1
+        Me.txtLRNo.MyLinkLable2 = Nothing
+        Me.txtLRNo.Name = "txtLRNo"
+        Me.txtLRNo.ReferenceFieldDesc = Nothing
+        Me.txtLRNo.ReferenceFieldName = Nothing
+        Me.txtLRNo.ReferenceTableName = Nothing
+        Me.txtLRNo.Size = New System.Drawing.Size(143, 18)
+        Me.txtLRNo.TabIndex = 46
+        '
+        'txtGRNo
+        '
+        Me.txtGRNo.CalculationExpression = Nothing
+        Me.txtGRNo.FieldCode = Nothing
+        Me.txtGRNo.FieldDesc = Nothing
+        Me.txtGRNo.FieldMaxLength = 0
+        Me.txtGRNo.FieldName = Nothing
+        Me.txtGRNo.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtGRNo.isCalculatedField = False
+        Me.txtGRNo.IsSourceFromTable = False
+        Me.txtGRNo.IsSourceFromValueList = False
+        Me.txtGRNo.IsUnique = False
+        Me.txtGRNo.Location = New System.Drawing.Point(106, 52)
+        Me.txtGRNo.MaxLength = 50
+        Me.txtGRNo.MendatroryField = False
+        Me.txtGRNo.MyLinkLable1 = Nothing
+        Me.txtGRNo.MyLinkLable2 = Nothing
+        Me.txtGRNo.Name = "txtGRNo"
+        Me.txtGRNo.ReferenceFieldDesc = Nothing
+        Me.txtGRNo.ReferenceFieldName = Nothing
+        Me.txtGRNo.ReferenceTableName = Nothing
+        Me.txtGRNo.Size = New System.Drawing.Size(143, 18)
+        Me.txtGRNo.TabIndex = 33
+        '
+        'RadLabel13
+        '
+        Me.RadLabel13.FieldName = Nothing
+        Me.RadLabel13.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadLabel13.Location = New System.Drawing.Point(13, 52)
+        Me.RadLabel13.Name = "RadLabel13"
+        Me.RadLabel13.Size = New System.Drawing.Size(41, 16)
+        Me.RadLabel13.TabIndex = 45
+        Me.RadLabel13.Text = "GR No"
+        '
+        'txtVehicleNo
+        '
+        Me.txtVehicleNo.CalculationExpression = Nothing
+        Me.txtVehicleNo.FieldCode = Nothing
+        Me.txtVehicleNo.FieldDesc = Nothing
+        Me.txtVehicleNo.FieldMaxLength = 0
+        Me.txtVehicleNo.FieldName = Nothing
+        Me.txtVehicleNo.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtVehicleNo.isCalculatedField = False
+        Me.txtVehicleNo.IsSourceFromTable = False
+        Me.txtVehicleNo.IsSourceFromValueList = False
+        Me.txtVehicleNo.IsUnique = False
+        Me.txtVehicleNo.Location = New System.Drawing.Point(106, 28)
+        Me.txtVehicleNo.MaxLength = 50
+        Me.txtVehicleNo.MendatroryField = False
+        Me.txtVehicleNo.MyLinkLable1 = Me.RadLabel5
+        Me.txtVehicleNo.MyLinkLable2 = Nothing
+        Me.txtVehicleNo.Name = "txtVehicleNo"
+        Me.txtVehicleNo.ReferenceFieldDesc = Nothing
+        Me.txtVehicleNo.ReferenceFieldName = Nothing
+        Me.txtVehicleNo.ReferenceTableName = Nothing
+        Me.txtVehicleNo.Size = New System.Drawing.Size(143, 18)
+        Me.txtVehicleNo.TabIndex = 43
+        '
+        'RadLabel5
+        '
+        Me.RadLabel5.FieldName = Nothing
+        Me.RadLabel5.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadLabel5.Location = New System.Drawing.Point(13, 28)
+        Me.RadLabel5.Name = "RadLabel5"
+        Me.RadLabel5.Size = New System.Drawing.Size(61, 16)
+        Me.RadLabel5.TabIndex = 44
+        Me.RadLabel5.Text = "Vehicle No"
+        '
+        'RadLabel1
+        '
+        Me.RadLabel1.FieldName = Nothing
+        Me.RadLabel1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadLabel1.Location = New System.Drawing.Point(13, 6)
+        Me.RadLabel1.Name = "RadLabel1"
+        Me.RadLabel1.Size = New System.Drawing.Size(49, 16)
+        Me.RadLabel1.TabIndex = 42
+        Me.RadLabel1.Text = "GRN No"
+        '
+        'txtDocNo
+        '
+        Me.txtDocNo.FieldName = Nothing
+        Me.txtDocNo.Location = New System.Drawing.Point(106, 3)
+        Me.txtDocNo.MendatroryField = False
+        Me.txtDocNo.MyCharacterCasing = System.Windows.Forms.CharacterCasing.Normal
+        Me.txtDocNo.MyFont = New System.Drawing.Font("Segoe UI", 8.25!)
+        Me.txtDocNo.MyLinkLable1 = Nothing
+        Me.txtDocNo.MyLinkLable2 = Nothing
+        Me.txtDocNo.MyMaxLength = 32767
+        Me.txtDocNo.MyReadOnly = False
+        Me.txtDocNo.Name = "txtDocNo"
+        Me.txtDocNo.Size = New System.Drawing.Size(252, 19)
+        Me.txtDocNo.TabIndex = 1
+        Me.txtDocNo.Value = ""
+        '
+        'frmCorrectionforWrongEntry
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "frmCorrectionforWrongEntry"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.Text = "frmCorrectionforWrongEntry"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.ResumeLayout(False)
+        CType(Me.RadPageView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.RadPageView1.ResumeLayout(False)
+        Me.RadPageViewPage1.ResumeLayout(False)
+        Me.RadPageViewPage1.PerformLayout()
+        CType(Me.MyLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtLRNo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtGRNo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtVehicleNo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents RadPageView1 As RadPageView
+    Friend WithEvents RadPageViewPage1 As RadPageViewPage
+    Friend WithEvents txtDocNo As common.UserControls.txtNavigator
+    Friend WithEvents RadLabel1 As common.Controls.MyLabel
+    Friend WithEvents txtVehicleNo As common.Controls.MyTextBox
+    Friend WithEvents RadLabel5 As common.Controls.MyLabel
+    Friend WithEvents txtGRNo As common.Controls.MyTextBox
+    Friend WithEvents RadLabel13 As common.Controls.MyLabel
+    Friend WithEvents MyLabel1 As common.Controls.MyLabel
+    Friend WithEvents txtLRNo As common.Controls.MyTextBox
+End Class
