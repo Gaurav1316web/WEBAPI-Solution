30d0ecfe1960b85cf38883ef7a97c306c40eae14
@tecxpertAlokYadav <alok.yadav@tecxpert.in>
2023-09-15 12:39:34 +0530

    HR and Payroll ---add Location

--- XpertERPHRandPayroll/HR and Payroll/Tranasction/frmEmployee_Salary.vb
+++ XpertERPHRandPayroll/HR and Payroll/Tranasction/frmEmployee_Salary.vb
@@ -523,7 +523,7 @@ Public Class frmEmployee_Salary
         Try
             Dim whrcls As String = Nothing
             Dim LocCode As String = Nothing
-            If clsCommon.myLen(objCommonVar.CurrentUserCode) > 0 Then
+            If clsCommon.myLen(objCommonVar.strCurrUserLocations) > 0 Then
                 LocCode = clsCommon.myCstr(clsDBFuncationality.getSingleValue("select isnull(TSPL_USER_MASTER.Default_Location,'') from TSPL_USER_MASTER Left Outer Join TSPL_LOCATION_MASTER on TSPL_USER_MASTER.Default_Location =TSPL_LOCATION_MASTER.Location_Code where 1=1 and TSPL_USER_MASTER.User_Code='" + objCommonVar.CurrentUserCode + "' "))
                 If clsCommon.myLen(LocCode) > 0 Then
                     whrcls = " LOCATION_CODE='" + LocCode + "'"
@@ -551,7 +551,7 @@ Public Class frmEmployee_Salary
         Try
             Dim whrcls As String = Nothing
             Dim LocCode As String = Nothing
-            If clsCommon.myLen(objCommonVar.CurrentUserCode) > 0 Then
+            If clsCommon.myLen(objCommonVar.strCurrUserLocations) > 0 Then
                 LocCode = clsCommon.myCstr(clsDBFuncationality.getSingleValue("select isnull(TSPL_USER_MASTER.Default_Location,'') from TSPL_USER_MASTER Left Outer Join TSPL_LOCATION_MASTER on TSPL_USER_MASTER.Default_Location =TSPL_LOCATION_MASTER.Location_Code where 1=1 and TSPL_USER_MASTER.User_Code='" + objCommonVar.CurrentUserCode + "' "))
                 If clsCommon.myLen(LocCode) > 0 Then
                     whrcls = " LOCATION_CODE='" + LocCode + "'"
@@ -586,7 +586,7 @@ Public Class frmEmployee_Salary
 
         Dim whrcls As String = Nothing
         Dim LocCode As String = Nothing
-        If clsCommon.myLen(objCommonVar.CurrentUserCode) > 0 Then
+        If clsCommon.myLen(objCommonVar.strCurrUserLocations) > 0 Then
             LocCode = clsCommon.myCstr(clsDBFuncationality.getSingleValue("select isnull(TSPL_USER_MASTER.Default_Location,'') from TSPL_USER_MASTER Left Outer Join TSPL_LOCATION_MASTER on TSPL_USER_MASTER.Default_Location =TSPL_LOCATION_MASTER.Location_Code where 1=1 and TSPL_USER_MASTER.User_Code='" + objCommonVar.CurrentUserCode + "' "))
             If clsCommon.myLen(LocCode) > 0 Then
                 whrcls = " Emp.LOCATION_CODE='" + LocCode + "'"
@@ -1168,7 +1168,7 @@ Public Class frmEmployee_Salary
         Try
             Dim whrcls As String = Nothing
             Dim LocCode As String = Nothing
-            If clsCommon.myLen(objCommonVar.CurrentUserCode) > 0 Then
+            If clsCommon.myLen(objCommonVar.strCurrUserLocations) > 0 Then
                 LocCode = clsCommon.myCstr(clsDBFuncationality.getSingleValue("select isnull(TSPL_USER_MASTER.Default_Location,'') from TSPL_USER_MASTER Left Outer Join TSPL_LOCATION_MASTER on TSPL_USER_MASTER.Default_Location =TSPL_LOCATION_MASTER.Location_Code where 1=1 and TSPL_USER_MASTER.User_Code='" + objCommonVar.CurrentUserCode + "' "))
                 If clsCommon.myLen(LocCode) > 0 Then
                     whrcls = " LOCATION_CODE='" + LocCode + "'"
