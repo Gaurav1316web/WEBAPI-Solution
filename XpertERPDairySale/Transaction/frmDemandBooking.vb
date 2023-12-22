5186f538b630e9d04dc98fce1686b86ed111360d
Vinu01 <vinodkumar15190@gmail.com>
2023-12-22 18:08:53 +0530

    fix Demand Booking

--- XpertERPDairySale/Transaction/frmDemandBooking.vb
+++ XpertERPDairySale/Transaction/frmDemandBooking.vb
@@ -3866,7 +3866,7 @@ from
             If dt IsNot Nothing And dt.Rows.Count > 0 Then
                 clsCommon.ProgressBarShow()
                 For Each dr As DataRow In dt.Rows
-                    AddNew()
+                    'AddNew()
                     QuickDemamd(clsCommon.GetPrintDate(dr.Item("DemandDate")), objCommonVar.CurrentUserCode, clsCommon.myCstr(dr.Item("ShiftType")), clsCommon.myCstr(dr.Item("Route_No")))
                 Next
                 AddNew()
