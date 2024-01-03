0c8c40b1f6b8414270ef8c90e5011d96a6be429c
@tecxpertAlokYadav <alok.yadav@tecxpert.in>
2024-01-03 11:58:36 +0530

    MCC Milk Register

--- XpertERPBulkProcurement/Milk Procurement/Report/FrmMCCMilkRegister.vb
+++ XpertERPBulkProcurement/Milk Procurement/Report/FrmMCCMilkRegister.vb
@@ -3650,7 +3650,7 @@ Public Class FrmMCCMilkRegister
 
     Private Sub rmDeleteLayout_Click(sender As Object, e As EventArgs) Handles rmDeleteLayout.Click
         clsGridLayout.DeleteData(PageSetupReport_ID, objCommonVar.CurrentUserCode)
-        common.clsCommon.MyMessageBoxShow(Me, "Layout Delete successfully", "Information", Me.Text)
+        common.clsCommon.MyMessageBoxShow(Me, "Layout Delete successfully", Me.Text)
     End Sub
 
     Private Sub rbtnMCCRouteVLCCAll_ToggleStateChanged(sender As Object, args As StateChangedEventArgs)
