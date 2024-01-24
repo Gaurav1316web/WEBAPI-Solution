abb8c382d86cfbb0c26c3297f92f35c40c3d2803
shaurya.kumar@tecxpert.in <shaurya.kumar@tecxpert.in>
2024-01-24 15:42:54 +0530

    Complete work Salarygeneration work- absent work and employee salary work- export and import error and

--- XpertERPHRandPayroll/HR and Payroll/Tranasction/frmSalaryGeneration.vb
+++ XpertERPHRandPayroll/HR and Payroll/Tranasction/frmSalaryGeneration.vb
@@ -450,6 +450,7 @@ Public Class frmSalaryGeneration
 
     Private Sub txtCode__MYValidating(ByVal sender As System.Object, ByVal e As System.EventArgs, ByVal isButtonClicked As System.Boolean) Handles txtCode._MYValidating
         Dim whrcls As String = Nothing
+
         Dim LocCode As String = Nothing
         If clsCommon.myLen(objCommonVar.strCurrUserLocations) > 0 Then
             LocCode = clsCommon.myCstr(clsDBFuncationality.getSingleValue("select isnull(TSPL_USER_MASTER.Default_Location,'') from TSPL_USER_MASTER Left Outer Join TSPL_LOCATION_MASTER on TSPL_USER_MASTER.Default_Location =TSPL_LOCATION_MASTER.Location_Code where 1=1 and TSPL_USER_MASTER.User_Code='" + objCommonVar.CurrentUserCode + "' "))
