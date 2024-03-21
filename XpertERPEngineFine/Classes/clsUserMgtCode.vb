a1109e7228e0e0fa413cc06f17eaf97ec7838358
Gaurav kumar Singh <tecxpert07@outlook.com>
2024-03-21 14:14:11 +0530

    Added new Screen TransactionWiseStock with CrystalReport.

--- XpertERPEngineFine/Classes/clsUserMgtCode.vb
+++ XpertERPEngineFine/Classes/clsUserMgtCode.vb
@@ -2403,6 +2403,7 @@ Public Class clsUserMgtCode
     Public Const rptrlPenaltyRegister As String = "RL-PEN-REG"
     Public Const rptPerformanceReport As String = "PERF-RPT"
     Public Const VehicleUnloadingReport As String = "VEH-UNL-RPT"
+    Public Const rptTransactionWiseStock As String = "TRAN-STCK"
     Public Const rptRMUnloading As String = "RM-UNL-RPT"
     Public Const RptPOAgainstDocument As String = "PO_AGT"
     Public Const frmBillChecklist As String = "PO_BCL"
