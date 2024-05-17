50aae647e82426d489e5f2ed1db5fa7a1f69d2cd
balwinder.singh@tecxpert.in <balwinder.singh@tecxpert.in>
2024-05-17 11:29:49 +0530

    Adding DBT Capping Form

--- XpertERP/MDI.vb
+++ XpertERP/MDI.vb
@@ -7914,7 +7914,7 @@ Public Class MDI
                         formShow(frm, strProgramCode, strProgramName, isOpenInMDI, strDocNo, IFTrueShowFormElseShowDialog)
                     Case clsUserMgtCode.DBTCappingCheck
                         If clsCommon.myCDecimal(clsFixedParameter.GetData(clsFixedParameterType.DBTMilkQtyCapping, clsFixedParameterCode.DBTMilkQtyCapping, Nothing)) > 0 Then
-                            frm = New frmDBTCapingCheck
+                            frm = New frmDBTCaping
                             formShow(frm, strProgramCode, strProgramName, isOpenInMDI, strDocNo, IFTrueShowFormElseShowDialog)
                         Else
                             clsCommon.MyMessageBoxShow("This feature is not for you")
