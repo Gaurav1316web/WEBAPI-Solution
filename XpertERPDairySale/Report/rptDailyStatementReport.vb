3fa5c4802b210ad265a34f54c1838a8417400f1e
DivyanshiSharma65 <divyanshi.sharma@tecxpert.in>
2024-06-17 13:13:54 +0530

    add changes on daily statement report

--- XpertERPDairySale/Report/rptDailyStatementReport.vb
+++ XpertERPDairySale/Report/rptDailyStatementReport.vb
@@ -17,26 +17,29 @@ Public Class rptDailyStatementReport
     Private Sub btnDetail_Click(sender As Object, e As EventArgs) Handles btnDetail.Click
         Dim query As String = ""
         Dim BaseQry As String = ""
-
-        BaseQry = ReturnQry()
-        If rbtnRouteWise.IsChecked Then
-            query = "  Select Cust_Code, '" + objCommonVar.CurrentUser + "' as UserName,route_no,max(route_desc)route_desc, sum(Final_Qty)Final_Qty, Sku_Seq,max(Phone2)Phone2,max(Phone1)Phone1,max(Circle_No)Circle_No, max(Comp_Name)Comp_Name,max(City_Code)City_Code,max(State)State, max(Add1)Add1,max(Add2)Add2,max(Pincode)Pincode,max(Fax)Fax,max(Date)Date,max(Customer_Name) as Customer_Name,Item_Code,max(Short_Description) AS Short_Description,max(Unit_Desc) as Unit_Desc
+        Try
+            BaseQry = ReturnQry()
+            If rbtnRouteWise.IsChecked Then
+                query = "  Select  Cust_Code, '" + objCommonVar.CurrentUser + "' as UserName,route_no,max(route_desc)route_desc, sum(Final_Qty)Final_Qty, Sku_Seq,max(Phone2)Phone2,max(Phone1)Phone1,max(Circle_No)Circle_No, max(Comp_Name)Comp_Name,max(City_Code)City_Code,max(State)State, max(Add1)Add1,max(Add2)Add2,max(Pincode)Pincode,max(Fax)Fax,max(Date)Date,max(Customer_Name) as Customer_Name,Item_Code,max(Short_Description) AS Short_Description,max(Unit_Desc) as Unit_Desc
 		,sum(TotalLtr_ItemWise) as TotalLtr_ItemWise,sum(ItemNetAmount) as TotalAmt_ItemWise,sum(ProdQ) as ProdQ ,SUM(MAmt) AS MAmt,SUM(PAmt) AS PAmt,(sum(isnull(MAmt,0))+sum(isnull(PAmt,0))) as [Total Amount]
         from (  " & Environment.NewLine & " " & BaseQry & ""
-        Else
-            query = "  Select Cust_Code, '" + objCommonVar.CurrentUser + "' as UserName,max(route_desc)route_desc, sum(Final_Qty)Final_Qty, Sku_Seq,max(Phone2)Phone2,max(Phone1)Phone1,max(Circle_No)Circle_No, max(Comp_Name)Comp_Name,max(City_Code)City_Code,max(State)State, max(Add1)Add1,max(Add2)Add2,max(Pincode)Pincode,max(Fax)Fax,max(Date)Date,max(Customer_Name) as Customer_Name,Item_Code,max(Short_Description) AS Short_Description,max(Unit_Desc) as Unit_Desc
+            Else
+                query = "  Select  1 as Group_Cust,  Cust_Code, '" + objCommonVar.CurrentUser + "' as UserName,max(route_desc)route_desc, sum(Final_Qty)Final_Qty, Sku_Seq,max(Phone2)Phone2,max(Phone1)Phone1,max(Circle_No)Circle_No, max(Comp_Name)Comp_Name,max(City_Code)City_Code,max(State)State, max(Add1)Add1,max(Add2)Add2,max(Pincode)Pincode,max(Fax)Fax,max(Date)Date,max(Customer_Name) as Customer_Name,Item_Code,max(Short_Description) AS Short_Description,max(Unit_Desc) as Unit_Desc
 		,sum(TotalLtr_ItemWise) as TotalLtr_ItemWise,sum(ItemNetAmount) as TotalAmt_ItemWise,sum(ProdQ) as ProdQ ,SUM(MAmt) AS MAmt,SUM(PAmt) AS PAmt,(sum(isnull(MAmt,0))+sum(isnull(PAmt,0))) as [Total Amount]
         from (  " & Environment.NewLine & " " & BaseQry & ""
-        End If
-        If rbtnDistributorWise.IsChecked Then
-            query += " XXXFirst.Cust_Code,	XXXFirst.Item_Code ,XXXFirst.Sku_Seq  order by Sku_Seq "
-        ElseIf rbtnRouteWise.IsChecked Then
-            query += " XXXFirst.Route_No, XXXFirst.Cust_Code,	XXXFirst.Item_Code ,XXXFirst.Sku_Seq  order by XXXFirst.Route_No ASC "
+            End If
+            If rbtnDistributorWise.IsChecked Then
+                query += " group by  XXXFirst.Cust_Code,	XXXFirst.Item_Code ,XXXFirst.Sku_Seq  "
+                query += " union all " & Environment.NewLine & "  Select  2 as Group_Cust,  'Department Sale' as Cust_Code, '" + objCommonVar.CurrentUser + "' as UserName,max(route_desc)route_desc, sum(Final_Qty)Final_Qty, Sku_Seq,max(Phone2)Phone2,max(Phone1)Phone1,max(Circle_No)Circle_No, max(Comp_Name)Comp_Name,max(City_Code)City_Code,max(State)State, max(Add1)Add1,max(Add2)Add2,max(Pincode)Pincode,max(Fax)Fax,max(Date)Date,'Department Sale' as Customer_Name,Item_Code,max(Short_Description) AS Short_Description,max(Unit_Desc) as Unit_Desc
+		,sum(TotalLtr_ItemWise) as TotalLtr_ItemWise,sum(ItemNetAmount) as TotalAmt_ItemWise,sum(ProdQ) as ProdQ ,SUM(MAmt) AS MAmt,SUM(PAmt) AS PAmt,(sum(isnull(MAmt,0))+sum(isnull(PAmt,0))) as [Total Amount]
+        from (  " & Environment.NewLine & " " & BaseQry & " and Credit_Customer = 'Y'  Group By  XXXFirst.Item_Code ,XXXFirst.Sku_Seq   order by Group_Cust  "
 
-        End If
+            ElseIf rbtnRouteWise.IsChecked Then
+                query += " group by  XXXFirst.Route_No, XXXFirst.Cust_Code,	XXXFirst.Item_Code ,XXXFirst.Sku_Seq  order by XXXFirst.Route_No ASC "
 
+            End If
 
-        Dim dt As DataTable = clsDBFuncationality.GetDataTable(query)
+            Dim dt As DataTable = clsDBFuncationality.GetDataTable(query)
             If dt IsNot Nothing AndAlso dt.Rows.Count > 0 Then
                 Dim frmCRV As New frmCrystalReportViewer()
                 If rbtnDistributorWise.IsChecked Then
@@ -47,10 +50,14 @@ Public Class rptDailyStatementReport
             Else
                 clsCommon.MyMessageBoxShow(Me, "No data found to display", Me.Text)
             End If
+        Catch ex As Exception
+            clsCommon.MyMessageBoxShow(Me, ex.Message, Me.Text)
+        End Try
+
     End Sub
     Private Function ReturnQry() As String
         Dim BaseQry As String = ""
-        BaseQry = "Select TSPL_DEMAND_BOOKING_MASTER.Route_No, TSPL_DEMAND_BOOKING_MASTER.ShiftType, case when TSPL_ITEM_MASTER.Is_Ambient = 1 then round((isnull(TSPL_DEMAND_BOOKING_DETAIL.Qty,0) *isnull(TSPL_ITEM_UOM_DETAIL.Conversion_Factor,1))/I.KG,2) 
+        BaseQry = "Select TSPL_DEMAND_BOOKING_MASTER.Route_No,TSPL_ROUTE_MASTER.Route_Desc,TSPL_CUSTOMER_MASTER.Credit_Customer, TSPL_DEMAND_BOOKING_MASTER.ShiftType, case when TSPL_ITEM_MASTER.Is_Ambient = 1 then round((isnull(TSPL_DEMAND_BOOKING_DETAIL.Qty,0) *isnull(TSPL_ITEM_UOM_DETAIL.Conversion_Factor,1))/I.KG,2) 
 		else (case when TSPL_ITEM_MASTER.Is_FreshItem = 1 then round((isnull(TSPL_DEMAND_BOOKING_DETAIL.Qty,0) *isnull(TSPL_ITEM_UOM_DETAIL.Conversion_Factor,1))/I.[LTR],2) else 0 end ) end as Final_Qty,case when TSPL_ITEM_MASTER.Is_FreshItem = 1 then	round((isnull(TSPL_DEMAND_BOOKING_DETAIL.Qty,0) *isnull(TSPL_ITEM_UOM_DETAIL.Conversion_Factor,1))/I.KG,2) else 0 end as Kg_Qty , tspl_company_master.State,tspl_company_master.City_Code,tspl_company_master.Circle_No,tspl_company_master.Phone1,tspl_company_master.Phone2,   tspl_company_master.Comp_Name,tspl_company_master.Add1,tspl_company_master.Add2,tspl_company_master.Pincode,tspl_company_master.Fax,'" & clsCommon.GetPrintDate(txtDate.Value, "dd/MMM/yyyy") & "' as Date, TSPL_ITEM_MASTER.Sku_Seq, TSPL_CUSTOMER_MASTER.Display_Seq,dist.Cust_Code,coalesce(TSPL_CUSTOMER_MASTER.Customer_Name_Hindi,TSPL_CUSTOMER_MASTER.Customer_Name) as Customer_Name        
 	    , TSPL_DEMAND_BOOKING_DETAIL.Item_Code,TSPL_ITEM_MASTER.Short_Description as Short_Description,TSPL_UNIT_MASTER.Unit_Desc,TSPL_DEMAND_BOOKING_DETAIL.TotalLtr_ItemWise,TSPL_DEMAND_BOOKING_DETAIL.ItemNetAmount
        ,TSPL_DEMAND_BOOKING_DETAIL.TotalCrates_ItemWise ,(CASE WHEN TSPL_ITEM_MASTER.Is_Milk_Pouch=0 THEN TSPL_DEMAND_BOOKING_DETAIL.Qty ELSE 0 END) as ProdQ,(CASE WHEN TSPL_ITEM_MASTER.Is_Milk_Pouch=1 THEN TSPL_DEMAND_BOOKING_DETAIL.ItemNetAmount ELSE 0 END) as MAmt
@@ -64,8 +71,7 @@ Public Class rptDailyStatementReport
         left join (  SELECT * FROM ( select item_code,uom_code,conversion_factor from TSPL_ITEM_UOM_DETAIL) I  PIVOT (Max(conversion_factor) FOR uom_code IN ( [KG],[LTR] )) P ) I ON TSPL_DEMAND_BOOKING_DETAIL.Item_Code = I.item_code 
         WHERE  convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date) = CONVERT(DATE, '" & clsCommon.GetPrintDate(txtDate.Value, "dd/MMM/yyyy") & "', 103)  ) XXXFirst 
    where Item_Code is  not null and Cust_Code is not null "
-   where Item_Code is  not null and Cust_Code is not null
-   Group By "
+        Return BaseQry
     End Function
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
         Me.Close()
@@ -96,5 +102,9 @@ Public Class rptDailyStatementReport
             Else
                 clsCommon.MyMessageBoxShow(Me, "No data found to display", Me.Text)
             End If
+        Catch ex As Exception
+            clsCommon.MyMessageBoxShow(Me, ex.Message, Me.Text)
+        End Try
+
     End Sub
 End Class
