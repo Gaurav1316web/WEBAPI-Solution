ac4cc265898288e841d29f2998e1652a1ee33d66
soniyacsana123@gmail.com <soniyacsana123@gmail.com>
2024-07-18 10:10:56 +0530

    Add New Crystal Report Zone Wise Report

--- XpertERPDairySale/Report/rptZoneWiseReport1.vb
+++ XpertERPDairySale/Report/rptZoneWiseReport1.vb
@@ -19,7 +19,6 @@ Public Class rptZoneWiseReport1
         LoadData()
     End Sub
     Sub Reset()
-        gv1.DataSource = Nothing
         RadPageView1.SelectedPage = RadPageViewPage1
         EnableDisableControl(True)
     End Sub
@@ -74,67 +73,8 @@ select * from (select  CASE WHEN CODE <> 'LTR'  THEN TotalCrates_ItemWise ELSE T
         End Try
     End Sub
 
-    Sub SetGridFormation()
-        gv1.TableElement.TableHeaderHeight = 40
-        gv1.MasterTemplate.ShowRowHeaderColumn = True
-        gv1.EnableFiltering = True
-        gv1.ShowRowHeaderColumn = True
-        gv1.ShowGroupPanel = False
-        For ii As Integer = 0 To gv1.Columns.Count - 1
-            gv1.Columns(ii).ReadOnly = True
-            gv1.Columns(ii).IsVisible = True
-            gv1.Columns(ii).FormatString = ""
-        Next
-        gv1.Columns("Total").IsVisible = False
-        gv1.Columns("Cust_Code").HeaderText = "Customer"
-        gv1.Columns("Customer_Name").HeaderText = "Customer Name"
-
-        Dim summaryRowItem As New GridViewSummaryRowItem()
-        For ii As Integer = 2 To gv1.Columns.Count - 1
-            summaryRowItem.Add(New GridViewSummaryItem(gv1.Columns(ii).Name, "", GridAggregateFunction.Sum))
-        Next
-        gv1.MasterTemplate.SummaryRowsBottom.Add(summaryRowItem)
-    End Sub
-
     Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
         Reset()
-    End Sub
-
-    Private Sub btnExcel_Click(sender As Object, e As EventArgs) Handles btnExcel.Click
-        Try
-            If gv1.Rows.Count > 0 Then
-                Dim arrHeader As List(Of String) = New List(Of String)()
-                arrHeader.Add("Print Date (" + clsCommon.GetPrintDate(clsCommon.GETSERVERDATE(), "dd-MMM-yyyy hh:mm:ss tt") + ")")
-                arrHeader.Add("Company : " & objCommonVar.CurrentCompanyName)
-                arrHeader.Add("Name : " & clsDBFuncationality.getSingleValue("select program_name from tspl_program_Master where program_cODE='" & clsUserMgtCode.rptD1D2Report & "'"))
-                arrHeader.Add("Date Range : " & clsCommon.GetPrintDate(txtFromDate.Value, "dd/MM/yyyy") + "  To " + clsCommon.GetPrintDate(txtToDate.Value, "dd/MM/yyyy"))
-
-                transportSql.QuickExportToExcel(gv1, "", Me.Text, , arrHeader)
-            Else
-                clsCommon.MyMessageBoxShow(Me, "No data found to export.", Me.Text)
-
-            End If
-        Catch ex As Exception
-            common.clsCommon.MyMessageBoxShow(Me, ex.Message, Me.Text)
-        End Try
-    End Sub
-
-    Private Sub btnPDF_Click(sender As Object, e As EventArgs) Handles btnPDF.Click
-        Try
-
-            If gv1.Rows.Count > 0 Then
-                Dim arrHeader As List(Of String) = New List(Of String)()
-
-                clsCommon.MyExportToPDF(Me.Text, gv1, arrHeader, Me.Text)
-                arrHeader.Add("Date Range: " + clsCommon.GetPrintDate(txtFromDate.Value, "dd/MM/yyyy") + " To " + clsCommon.GetPrintDate(txtToDate.Value, "dd/MM/yyyy"))
-
-            Else
-                clsCommon.MyMessageBoxShow(Me, "No data found To export", Me.Text)
-            End If
-
-        Catch ex As Exception
-            common.clsCommon.MyMessageBoxShow(Me, ex.Message, Me.Text)
-        End Try
     End Sub
 
     Private Sub btnPrint_Click_1(sender As Object, e As EventArgs) Handles btnPrint.Click
