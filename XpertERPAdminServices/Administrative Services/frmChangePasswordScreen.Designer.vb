7dca9cf2ac55d321f17c61b031c9279f4a99777b
MohdSuhail <mohd.suhail@tecxpert.in>
2024-09-04 12:30:24 +0530

    merge

---
+++ XpertERPAdminServices/Administrative Services/frmChangePasswordScreen.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmChangePasswordScreen
+    Inherits FrmMainTranScreen
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmChangePasswordScreen))
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.chkShowPass = New System.Windows.Forms.CheckBox()
+        Me.lblLength = New common.Controls.MyLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtOldPass = New System.Windows.Forms.TextBox()
+        Me.txtNewPass = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCNFPass = New System.Windows.Forms.TextBox()
+        Me.btnReset = New Telerik.WinControls.UI.RadButton()
+        Me.btnClose = New Telerik.WinControls.UI.RadButton()
+        Me.rbtnSave = New Telerik.WinControls.UI.RadButton()
+        Me.lblHeader = New System.Windows.Forms.Label()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.lblLength, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnReset, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnClose, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.rbtnSave, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.lblHeader)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.chkShowPass)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.lblLength)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label3)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txtOldPass)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txtNewPass)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txtCNFPass)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnReset)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnClose)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.rbtnSave)
+        Me.SplitContainer1.Size = New System.Drawing.Size(691, 452)
+        Me.SplitContainer1.SplitterDistance = 417
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'chkShowPass
+        '
+        Me.chkShowPass.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.chkShowPass.AutoSize = True
+        Me.chkShowPass.Location = New System.Drawing.Point(202, 259)
+        Me.chkShowPass.Name = "chkShowPass"
+        Me.chkShowPass.Size = New System.Drawing.Size(107, 17)
+        Me.chkShowPass.TabIndex = 16
+        Me.chkShowPass.Text = "Show Password"
+        Me.chkShowPass.UseVisualStyleBackColor = True
+        '
+        'lblLength
+        '
+        Me.lblLength.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.lblLength.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.lblLength.FieldName = Nothing
+        Me.lblLength.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.lblLength.Location = New System.Drawing.Point(85, 304)
+        Me.lblLength.Name = "lblLength"
+        Me.lblLength.Size = New System.Drawing.Size(533, 46)
+        Me.lblLength.TabIndex = 15
+        Me.lblLength.Text = resources.GetString("lblLength.Text")
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(82, 165)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Old Password"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(82, 235)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Confirm Password"
+        '
+        'txtOldPass
+        '
+        Me.txtOldPass.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.txtOldPass.Location = New System.Drawing.Point(202, 163)
+        Me.txtOldPass.Name = "txtOldPass"
+        Me.txtOldPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtOldPass.Size = New System.Drawing.Size(275, 20)
+        Me.txtOldPass.TabIndex = 1
+        '
+        'txtNewPass
+        '
+        Me.txtNewPass.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.txtNewPass.Location = New System.Drawing.Point(202, 197)
+        Me.txtNewPass.Name = "txtNewPass"
+        Me.txtNewPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtNewPass.Size = New System.Drawing.Size(275, 20)
+        Me.txtNewPass.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(82, 200)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Create New Password"
+        '
+        'txtCNFPass
+        '
+        Me.txtCNFPass.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.txtCNFPass.Location = New System.Drawing.Point(202, 232)
+        Me.txtCNFPass.Name = "txtCNFPass"
+        Me.txtCNFPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtCNFPass.Size = New System.Drawing.Size(275, 20)
+        Me.txtCNFPass.TabIndex = 5
+        '
+        'btnReset
+        '
+        Me.btnReset.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnReset.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.Location = New System.Drawing.Point(81, 5)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(68, 21)
+        Me.btnReset.TabIndex = 5
+        Me.btnReset.Text = "Reset"
+        '
+        'btnClose
+        '
+        Me.btnClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnClose.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClose.Location = New System.Drawing.Point(611, 5)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(68, 21)
+        Me.btnClose.TabIndex = 4
+        Me.btnClose.Text = "Close"
+        '
+        'rbtnSave
+        '
+        Me.rbtnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.rbtnSave.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbtnSave.Location = New System.Drawing.Point(12, 5)
+        Me.rbtnSave.Name = "rbtnSave"
+        Me.rbtnSave.Size = New System.Drawing.Size(68, 21)
+        Me.rbtnSave.TabIndex = 3
+        Me.rbtnSave.Text = "Save"
+        '
+        'lblHeader
+        '
+        Me.lblHeader.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.lblHeader.AutoSize = True
+        Me.lblHeader.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeader.Location = New System.Drawing.Point(202, 94)
+        Me.lblHeader.Name = "lblHeader"
+        Me.lblHeader.Size = New System.Drawing.Size(43, 15)
+        Me.lblHeader.TabIndex = 17
+        Me.lblHeader.Text = "Label4"
+        '
+        'frmChangePasswordScreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(691, 452)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "frmChangePasswordScreen"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.Text = "frmChangePasswordScreen"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.ResumeLayout(False)
+        CType(Me.lblLength, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnReset, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnClose, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.rbtnSave, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents btnClose As RadButton
+    Friend WithEvents rbtnSave As RadButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtOldPass As TextBox
+    Friend WithEvents txtNewPass As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCNFPass As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblLength As common.Controls.MyLabel
+    Friend WithEvents btnReset As RadButton
+    Friend WithEvents chkShowPass As CheckBox
+    Friend WithEvents lblHeader As Label
+End Class
---
+++ XpertERPBulkProcurement/Milk Procurement/Transaction/FrmSendDBTToJanaadhar.vb
@@ -0,0 +1,127 @@
+﻿Imports common
+Imports System.Data.SqlClient
+
+Public Class FrmSendDBTToJanaadhar
+    Inherits FrmMainTranScreen
+#Region "Variables"
+    Dim ButtonTooltip As New ToolTip()
+#End Region
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+    Private Sub FrmMilkVSPPayment_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ButtonTooltip.SetToolTip(btnClose, "Press Alt+C for Close the Window")
+        ButtonTooltip.SetToolTip(btnGenerateBill, "Press Alt+R for Refresh the Data")
+        SetUserMgmtNew()
+        LoadUnion()
+    End Sub
+    Public Sub LoadUnion()
+        Dim qry As String = "select Code,Location_Name as Name from TSPL_MASTER.dbo.TSPL_APP_LOCATION where isnull(Union_Report,0)=1 order by Name"
+        Dim dt As DataTable = clsDBFuncationality.GetDataTable(qry)
+        cboUnion.DataSource = dt
+        cboUnion.ValueMember = "Code"
+        cboUnion.DisplayMember = "Name"
+    End Sub
+    Private Sub SetUserMgmtNew()
+        If Not (MyBase.isReadFlag) Then
+            Throw New Exception("Permission Denied")
+        End If
+        btnGenerateBill.Visible = MyBase.isPostFlag
+
+    End Sub
+    Private Sub btnGenerateBill_Click(sender As Object, e As EventArgs) Handles btnGenerateBill.Click
+        Try
+            If clsCommon.myLen(cboUnion.SelectedValue) <= 0 Then
+                cboUnion.Focus()
+                Throw New Exception("Please select Union")
+            End If
+            SendRemaingDBTToJanAadhaar("Sending DBT To JanAadhaar of [" + clsCommon.myCstr(cboUnion.Text) + "] ", clsCommon.myCstr(cboUnion.SelectedValue))
+        Catch ex As Exception
+            clsCommon.MyMessageBoxShow(Me, ex.Message, Me.Text)
+        End Try
+    End Sub
+    Private Sub SendRemaingDBTToJanAadhaar(ByVal Task As String, ByVal PortNo As String)
+        clsCommon.ProgressBarPercentShow()
+        Try
+            Dim qry As String = "select DataBase_Name  from TSPL_MASTER.dbo.TSPL_APP_LOCATION  where Code='" + PortNo + "'"
+            Dim DBName As String = clsCommon.myCstr(clsDBFuncationality.getSingleValue(qry))
+
+            Dim ResponceSucess As Integer = 0
+            Dim ResponceFailure As Integer = 0
+            qry = "select TSPL_MP_INCENTIVE_ENTRY_DETAIL.MP_Code,TSPL_MP_MASTER.JA_janaadhaarId,TSPL_MP_MASTER.JA_jan_mid,TSPL_DBT_NEFT_DETAIL.MP_Account_No,TSPL_DBT_NEFT_DETAIL.MP_IFSC_No,TSPL_DBT_NEFT_DETAIL.PK_Id,convert(varchar, TSPL_DBT_NEFT_BANK_RESPONSE.Created_Date,103) as Created_Date,TSPL_DBT_NEFT_DETAIL.Amount" & Environment.NewLine & ",(case when TSPL_DBT_NEFT_REJECT_DETAIL.PK_Id is not null then 'Failure' else (case when Bank_Response like 'STATUS : SUCCESS%' then 'Success' else 'Failure' end) end) as Bank_Response 
+from " + DBName + ".dbo.TSPL_DBT_NEFT_DETAIL 
+left outer join " + DBName + ".dbo.TSPL_MP_INCENTIVE_ENTRY_DETAIL on " + DBName + ".dbo.TSPL_MP_INCENTIVE_ENTRY_DETAIL.PK_Id = " + DBName + ".dbo.TSPL_DBT_NEFT_DETAIL.Against_MP_Incentive_TR
+left outer join " + DBName + ".dbo.TSPL_MP_MASTER on  " + DBName + ".dbo.TSPL_MP_MASTER.MP_Code = " + DBName + ".dbo.TSPL_MP_INCENTIVE_ENTRY_DETAIL.MP_Code
+inner join " + DBName + ".dbo.TSPL_DBT_NEFT_BANK_RESPONSE on " + DBName + ".dbo.TSPL_DBT_NEFT_BANK_RESPONSE.Ref_PK_Id = " + DBName + ".dbo.TSPL_DBT_NEFT_DETAIL.PK_Id
+left outer join " + DBName + ".dbo.TSPL_DBT_NEFT_REJECT_DETAIL on " + DBName + ".dbo.TSPL_DBT_NEFT_REJECT_DETAIL.Against_DBT_NEFT_TR= " + DBName + ".dbo.TSPL_DBT_NEFT_DETAIL.PK_Id
+where isnull(" + DBName + ".dbo.TSPL_MP_MASTER.Jan_Aadhar_No_Verified,0)=1 and ISNULL(" + DBName + ".dbo.TSPL_DBT_NEFT_BANK_RESPONSE.JA_Is_Saved,'N')='N'"
+            Dim dt As DataTable = clsDBFuncationality.GetDataTable(qry)
+            If dt IsNot Nothing AndAlso dt.Rows.Count > 0 Then
+                Dim ii As Int32 = 0
+                For Each dr As DataRow In dt.Rows
+                    Try
+                        ii += 1
+                        qry = clsCommon.myCstr(clsDBFuncationality.getSingleValue("select isnull(JA_Is_Saved,'N') as JA_Is_Saved from " + DBName + ".dbo.TSPL_DBT_NEFT_BANK_RESPONSE where Ref_PK_Id=" & clsCommon.myCstr(dr("PK_Id")) & ""))
+                        If clsCommon.CompairString(clsCommon.myCstr(qry), "N") = CompairStringResult.Equal Then
+                            Dim obj As clsXMLSendDBTDataToJanAadharHead = New clsXMLSendDBTDataToJanAadharHead()
+                            obj.Transaction = New clsXMLSendDBTDataToJanAadharDetail()
+                            obj.Transaction.EntitlementId = PortNo & clsCommon.myCstr(dr("MP_Code"))
+                            obj.Transaction.EntitlementMemId = ""
+                            obj.Transaction.JanaadhaarId = clsCommon.myCstr(dr("JA_janaadhaarId"))
+                            obj.Transaction.JanaadhaarMemId = clsCommon.myCstr(dr("JA_jan_mid"))
+                            obj.Transaction.TransactionId = PortNo & clsCommon.myCstr(dr("PK_Id"))
+                            obj.Transaction.DueTransactionId = PortNo & clsCommon.myCstr(dr("PK_Id"))
+                            obj.Transaction.AadharNo = ""
+                            obj.Transaction.Eid = ""
+                            obj.Transaction.BankAccNo = clsCommon.myCstr(dr("MP_Account_No"))
+                            obj.Transaction.Ifsc = clsCommon.myCstr(dr("MP_IFSC_No"))
+                            obj.Transaction.Micr = ""
+                            obj.Transaction.PaymentAmount = clsCommon.myCstr(dr("Amount"))
+                            obj.Transaction.PaymentDate = clsCommon.myCstr(dr("Created_Date"))
+                            obj.Transaction.Status = clsCommon.myCstr(dr("Bank_Response"))
+                            Dim flag As Boolean = False
+                            Dim objReturn As clsXMLSendDBTDataToJanAadharResponseRoot = clsSendDBTDataToJanAadhar.SendData(obj)
+                            If objReturn IsNot Nothing Then
+                                Dim hashtable As Hashtable = New Hashtable()
+                                clsCommon.AddColumnsForChange(hashtable, "JA_Request_ID", objReturn.RequestId)
+                                clsCommon.AddColumnsForChange(hashtable, "JA_CMSG", objReturn.Cmsg)
+
+                                If objReturn.Transaction IsNot Nothing Then
+                                    clsCommon.AddColumnsForChange(hashtable, "JA_Is_Saved", objReturn.Transaction.IsSaved)
+                                    clsCommon.AddColumnsForChange(hashtable, "JA_Msg", objReturn.Transaction.Msg)
+                                    flag = (clsCommon.CompairString(objReturn.Transaction.IsSaved, "Y") = CompairStringResult.Equal)
+                                End If
+                                clsCommon.AddColumnsForChange(hashtable, "JA_Created_Date", clsCommon.GetPrintDate(clsCommon.GETSERVERDATE(), "dd/MMM/yyyy hh:mm:ss tt"))
+                                clsCommon.AddColumnsForChange(hashtable, "JA_Created_By", objCommonVar.CurrentUserCode)
+                                clsCommonFunctionality.UpdateDataTable(hashtable, "" + DBName + ".dbo.TSPL_DBT_NEFT_BANK_RESPONSE", OMInsertOrUpdate.Update, "Ref_PK_Id=" & clsCommon.myCstr(dr("PK_Id")) & "")
+                            End If
+                            If flag Then
+                                ResponceSucess += 1
+                            Else
+                                ResponceFailure += 1
+                            End If
+                        End If
+                        clsCommon.ProgressBarPercentUpdate((ii + 1), dt.Rows.Count, "Sending DBT Data To JanAadhar Server.Success [" + clsCommon.myCstr(ResponceSucess) + "] And Failure [" + clsCommon.myCstr(ResponceFailure) + "]")
+                    Catch ex As Exception
+                        Throw New Exception(ex.Message)
+                    End Try
+                Next
+                clsCommon.ProgressBarPercentHide()
+            End If
+        Catch ex As Exception
+            clsCommon.ProgressBarPercentHide()
+            Throw New Exception(ex.Message)
+        End Try
+    End Sub
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        CloseForm()
+    End Sub
+    Private Sub CloseForm()
+        Me.Close()
+    End Sub
+    Private Sub FrmDBTPayment_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.Alt AndAlso e.KeyCode = Keys.C AndAlso btnClose.Enabled Then
+            CloseForm()
+        End If
+    End Sub
+End Class
