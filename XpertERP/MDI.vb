2533342598584b6de6b589c1f3f137be8e5a137a
balwinder.singh@tecxpert.in <balwinder.singh@tecxpert.in>
2024-09-09 09:49:58 +0530

    Print work of Demand

--- XpertERP/MDI.vb
+++ XpertERP/MDI.vb
@@ -375,7 +375,7 @@ Public Class MDI
 
         Try
             Dim strTempVersion As String = FileVersionInfo.GetVersionInfo(Application.StartupPath + "\XpertCommon.dll").FileVersion
-            If Not clsCommon.CompairString(strTempVersion, "2.1.6.76") = CompairStringResult.Equal Then
+            If Not clsCommon.CompairString(strTempVersion, "2.1.6.77") = CompairStringResult.Equal Then
                 Throw New Exception("Wrong DLL Version" + Environment.NewLine + "XpertCommon ")
             End If
             strTempVersion = FileVersionInfo.GetVersionInfo(Application.StartupPath + "\XpertERPBlankTableScript.dll").FileVersion
