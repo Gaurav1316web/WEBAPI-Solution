2f00468671ec7c7c38f63a4203dfe2e6544bdad3
Gaurav Kumar Singh <tecxpert071@outlook.com>
2024-11-19 15:00:10 +0530

    .

--- XpertERPDairySale/Report/rptBoothTruckSheet.vb
+++ XpertERPDairySale/Report/rptBoothTruckSheet.vb
@@ -436,25 +436,29 @@ where 2 = 2  "
             Dim DedCode As String = Nothing
             Dim itemamt As String = Nothing
             Dim dtitemName As DataTable = Nothing
-            itemqry = " select TSPL_DEMAND_BOOKING_DETAIL.Item_Code,max(Alies_Name)Alies_Name
+            itemqry = " select TSPL_DEMAND_BOOKING_DETAIL.Item_Code,max(Alies_Name)Alies_Name,max(Sku_Seq)Sku_Seq
                         from TSPL_DEMAND_BOOKING_DETAIL
                         left outer join TSPL_DEMAND_BOOKING_MASTER on TSPL_DEMAND_BOOKING_MASTER.Document_No=TSPL_DEMAND_BOOKING_DETAIL.Document_No
                         left outer join TSPL_ITEM_MASTER ON TSPL_ITEM_MASTER.Item_Code=TSPL_DEMAND_BOOKING_DETAIL.Item_Code 
                         left outer join TSPL_ROUTE_MASTER ON TSPL_ROUTE_MASTER.Route_No=TSPL_DEMAND_BOOKING_MASTER.Route_No
-                        WHERE 2=2 and Cast(TSPL_DEMAND_BOOKING_MASTER.Document_Date as Date) >='" + FromDate + "' and Cast(TSPL_DEMAND_BOOKING_MASTER.Document_Date as Date) <='" + TODate + "'
-                        AND TSPL_DEMAND_BOOKING_MASTER.Route_No IN ('" + txtRouteCode.Value + "')
-                        group by TSPL_DEMAND_BOOKING_DETAIL.Item_Code   order by Item_Code "
+                        WHERE 2=2 --and Cast(TSPL_DEMAND_BOOKING_MASTER.Document_Date as Date) >='" + clsCommon.GetPrintDate((FromDate), "dd/MMM/yyyy") + "' and Cast(TSPL_DEMAND_BOOKING_MASTER.Document_Date as Date) <='" + clsCommon.GetPrintDate((TODate), "dd/MMM/yyyy") + "'
+                        --AND TSPL_DEMAND_BOOKING_MASTER.Route_No IN ('" + txtRouteCode.Value + "')
+                        and TSPL_ITEM_MASTER.Is_FreshItem = 1
+                        group by TSPL_DEMAND_BOOKING_DETAIL.Item_Code   --order by Sku_Seq "
             dtitemName = clsDBFuncationality.GetDataTable(itemqry)
             If dtitemName.Rows.Count > 0 Then
                 For i As Integer = 0 To dtitemName.Rows.Count - 1
                     If clsCommon.myLen(DedCode) > 0 AndAlso Not DedCode.Contains(clsCommon.myCstr(dtitemName.Rows(i)("Item_Code"))) Then
-                        DedCode += "," + clsCommon.myCstr(dtitemName.Rows(i)("Item_Code"))
+                        DedCode += "," + "case when ItemCode= '" + clsCommon.myCstr(dtitemName.Rows(i)("Item_Code")) + "' then sum(Qty) end as [" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "] "
                         itemName += "," + "Sum(IsNull([" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "],0)) As [" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "]"
                         itemNames += "," + "Sum(IsNull([" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "],0)) As [" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "]"
                         itemName1 += "," + "[" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "]"
                         itemamt += "+(IsNull([" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "],0))"
-                    ElseIf Not DedCode.Contains(clsCommon.myCstr(dtitemName.Rows(i)("Item_Code"))) Then
-                        DedCode = clsCommon.myCstr(dtitemName.Rows(i)("Item_Code"))
+                        'ElseIf Not DedCode.Contains(clsCommon.myCstr(dtitemName.Rows(i)("Item_Code"))) Then
+                    Else
+
+                        DedCode = " ,case when ItemCode= '" + clsCommon.myCstr(dtitemName.Rows(i)("Item_Code")) + "' then sum(Qty) end as [" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "]"
+                        ' DedCode = clsCommon.myCstr(dtitemName.Rows(i)("Item_Code"))
                         itemName = ",Sum(IsNull([" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "],0)) As [" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "]"
                         itemNames = ",Sum(IsNull([" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "],0)) As [" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "]"
                         itemName1 = "[" + clsCommon.myCstr(dtitemName.Rows(i)("Alies_Name")) + "]"
@@ -493,26 +497,19 @@ where 2 = 2  "
             End If
             Dim qry As String = " select "
             If PrintPouchCrateQtyOnPrint = True Then
-                qry += "  CASE WHEN (IsNull(Crateqty,0))=0 and (CrateCF)<(Loosepouch)  then (Unit_code) else 'CRATE' + ' ' + (Unit_code) end as UnitCode,* from (select  max(PouchCF)PouchCF,max(CrateCF)CrateCF,sum(PouchQty)PouchQty, SUM(COALESCE(CRATE, 0) + COALESCE(Crateqty, 0)) AS crateqty,
-                         case when sum(CrateCF) is null then sum(Qty) else SUM(COALESCE(PouchQtys, 0)) - SUM(COALESCE(CrateCF, 0) * COALESCE(Crateqty, 0)) end AS Loosepouch,
-                         max(Access_Officer)Access_Officer,max(Comp_Code1)Comp_Code1,max(Is_FreshItem)Is_FreshItem,max(Is_Ambient)Is_Ambient,max(Description)Description,max(Vehicle_Id)Vehicle_Id,max(FromDate)FromDate,max(ToDate)ToDate,max(FromShift)FromShift,max(Toshift)Toshift,max(ShiftType)ShiftType,max(Comp_Name)Comp_Name,max(Transporter_Name)Transporter_Name,max(Add1)Add1,max(City_Code)City_Code,max(Pincode)Pincode,max(State)State,max(Phone1)Phone1
-                        ,max(Cust_Code)Cust_Code,max(ItemCode)ItemCode,max(Short_Description)Short_Description,max(BoothName)BoothName,max(Route_No)Route_No,
-                        max(Route_Desc)Route_Desc,max(Shift_Type)Shift_Type,max(Document_Date)Document_Date,max(Item_Desc)Item_Desc,sum(Amount)Amount,
-                        max(Item_Description)Item_Description,max(Unit_code)Unit_code,CASE WHEN Sum(IsNull(Crateqty,0))=0 then max(Unit_code) else 'CRATE' + ' ' + max(Unit_code) end as UnitCode
-,sum(CRATE)CRATE,sum(Qty)Qty,max(Sku_Seq)Sku_Seq,sum(Pouch)Pouch,sum(Receipt_Amount)Receipt_Amount,
-                        sum(Pack)Pack from (select floor(Qty/PouchCF) as PouchQty,
-                        FLOOR(Qty/CrateCF) as Crateqty,
-                        floor(Case When xx.Unit_code='Pouch' Then Pouch/PouchCF else 0 end )as PouchQtys,* from "
+                qry += " max(PouchCF)PouchCF,max(CrateCF)CrateCF,Cust_Code,MAX(BoothName)BoothName,max(Route_No)Route_No,sum(Amount)Amount,max(Unit_code)Unit_Code " + itemName + "
+                         FROM 
+                        (Select max(PouchCF)PouchCF,max(CrateCF)CrateCF,Cust_Code,ItemCode,Cust_Code+ '  ' + max(Customer_Name)  as [BoothName]
+                        ,max(Route_No)Route_No,sum(Amount)Amount,max(Unit_code)Unit_Code " + DedCode + " From "
             Else
                 qry += " * from "
             End If
-
 
             qry += " 
 ( SELECT ItemConversionInPouch.Conversion_Factor as PouchCF ,ItemConversionCrate.Conversion_Factor AS CrateCF,
 TSPL_COMPANY_MASTER.Logo_Img2,TSPL_COMPANY_MASTER.Logo_Img,Access_officer,Comp_Code1,Is_FreshItem,Is_Ambient,TSPL_VEHICLE_MASTER.Description,Vehicle_Id,'" + FromDate + "' AS FromDate, '" + TODate + " ' as ToDate,'" + Fromshift + "' AS FromShift, '" + Toshift + " ' as Toshift,TSPL_DEMAND_BOOKING_MASTER.ShiftType,TSPL_COMPANY_MASTER.Comp_Name ,tspl_transport_master.Transporter_Name,TSPL_COMPANY_MASTER.Add1,TSPL_COMPANY_MASTER.City_Code,TSPL_COMPANY_MASTER.Pincode,TSPL_COMPANY_MASTER.State,TSPL_COMPANY_MASTER.Phone1 ,TSPL_DEMAND_BOOKING_DETAIL.Cust_Code,TSPL_ITEM_MASTER.Item_Code as ItemCode  ,"
             If rdbEnglish.IsChecked = True Then
-                qry += "(TSPL_ITEM_MASTER.Alies_Name)Short_Description,TSPL_DEMAND_BOOKING_DETAIL.Cust_Code + ' ' + TSPL_CUSTOMER_MASTER.Customer_Name  as [BoothName], "
+                qry += "(TSPL_ITEM_MASTER.Alies_Name)Short_Description,TSPL_CUSTOMER_MASTER.Customer_Name,TSPL_DEMAND_BOOKING_DETAIL.Cust_Code + ' ' + TSPL_CUSTOMER_MASTER.Customer_Name  as [BoothName], "
             ElseIf rdbHindi.IsChecked = True Then
                 ' qry += " (TSPL_ITEM_MASTER.Alies_Name)Short_Description, (TSPL_ITEM_MASTER.Alies_Name_Hindi)  as Item_Description,  "
                 qry += " (TSPL_ITEM_MASTER.Alies_Name_Hindi)Short_Description,TSPL_DEMAND_BOOKING_DETAIL.Cust_Code  + ' ' + TSPL_CUSTOMER_MASTER.Customer_Name_Hindi  as [BoothName], "
@@ -574,7 +571,7 @@ left outer join TSPL_ZONE_MASTER on TSPL_ZONE_MASTER.zone_code = TSPL_CUSTOMER_M
             qry += " And TSPL_DEMAND_BOOKING_master.Route_No In ('" + clsCommon.myCstr(txtRouteCode.Value) + "') "
 
             If PrintPouchCrateQtyOnPrint = True Then
-                qry += " ) xx)xxy group by ItemCode,Cust_Code)zz order by Sku_Seq "
+                qry += " ) xx group by XX.Cust_Code, xx.ItemCode )xy group by XY.Cust_Code "
             Else
                 qry += " ) xx order by Sku_Seq "
             End If
