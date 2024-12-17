c676801ba0a5fabb233ea68c9b83b7854860ba86
shaurya.kumar@tecxpert.in <shaurya.kumar@tecxpert.in>
2024-12-17 10:38:36 +0530

    Complete work add new report Booth demand report By Parbhat Sir

--- XpertERPDairySale/Report/FrmBoothDemandReport.vb
+++ XpertERPDairySale/Report/FrmBoothDemandReport.vb
@@ -4,13 +4,14 @@ Public Class FrmBoothDemandReport
     Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
         txtfDate.Value = clsCommon.GETSERVERDATE()
         txtToDate.Value = clsCommon.GETSERVERDATE()
+        TxtRoute.arrValueMember = Nothing
         gvData.DataSource = Nothing
         RadPageView1.SelectedPage = RadPageViewPage1
         rbtnDetail.IsChecked = True
         rbtnSummary.IsChecked = False
-        rdbMilk.Checked = True
+        rdbMilk.Checked = False
         rdbProduct.Checked = False
-        rdbDemandBoth.Checked = False
+        rdbDemandBoth.Checked = True
     End Sub
 
     Private Sub rmenuPDF_Click(sender As Object, e As EventArgs)
@@ -92,107 +93,137 @@ Public Class FrmBoothDemandReport
             ElseIf rdbDemandBoth.Checked Then
                 Whr += " and TSPL_DEMAND_BOOKING_MASTER.ItemType='Both' "
             End If
-            If rbtnSummary.IsChecked Then
+
+            If TxtRoute.arrValueMember IsNot Nothing AndAlso TxtRoute.arrValueMember.Count > 0 Then
+                Whr += " and TSPL_DEMAND_BOOKING_MASTER.route_no In (" + clsCommon.GetMulcallString(TxtRoute.arrValueMember) + ")"
+            End If
+
+            If rbtnDetail.IsChecked Then
                 GetReportGridID()
-                qry = "SELECT distinct
-                [Date], [Both Code], [Customer Name], [Mobile No], [Route], [Route Name],[Item_Code],[Unit_code], [Shift],
-                [Tonned Milk 500 Ml] AS [TM500], [Tonned Milk 1 LT] AS [TM1LT], [Standard Milk 500 Ml] AS [SM500],
-                [Standard Milk 1 LTR] AS [SM1LT], [GOLD Milk 500 Ml] AS [GM500], [GOLD Milk 1 LT] AS [GM1LT],
-                [PLAIN CHHACH 1/2 LTR] AS [CHHACH], [Tonned Milk 6 LT] AS [TM6LT], [GOLD Milk 6 LT] AS [GM6LT],
-                [COW MILK 500 Ml] AS [COW500]
-                FROM (
-                SELECT 
-                    CASE WHEN ITEMDETAIL1.Report_UOM=1 THEN ((Qty *isnull((TSPL_ITEM_UOM_DETAIL.Conversion_Factor),1))/ITEMDETAIL1.Conversion_Factor) end as [Quantity],
-                    CASE 
-                        WHEN ITEMDETAIL1.Report_UOM = 1 THEN ITEMDETAIL1.UOM_Code 
-                    END AS [UOM],
-                    CONVERT(VARCHAR, TSPL_DEMAND_BOOKING_MASTER.Document_Date, 103) AS [Date],
-                    TSPL_DEMAND_BOOKING_DETAIL.Cust_Code AS [Both Code],
-                    TSPL_CUSTOMER_MASTER.Customer_Name AS [Customer Name],
-                    TSPL_CUSTOMER_MASTER.Phone1 AS [Mobile No],
-                    TSPL_DEMAND_BOOKING_MASTER.Route_No AS [Route],
-                    TSPL_CUSTOMER_MASTER.Route_Desc AS [Route Name],
-                    TSPL_DEMAND_BOOKING_MASTER.ShiftType AS [Shift],
-		            tspl_item_master.Item_Code,
-                    tspl_item_master.Item_Desc,
-                    TSPL_DEMAND_BOOKING_DETAIL.Qty,
-		            TSPL_DEMAND_BOOKING_DETAIL.Unit_code
-                FROM TSPL_DEMAND_BOOKING_MASTER
-                LEFT JOIN TSPL_DEMAND_BOOKING_DETAIL 
-                    ON TSPL_DEMAND_BOOKING_DETAIL.Document_No = TSPL_DEMAND_BOOKING_MASTER.Document_No
-                LEFT JOIN TSPL_CUSTOMER_MASTER 
-                    ON TSPL_CUSTOMER_MASTER.Cust_Code = TSPL_DEMAND_BOOKING_DETAIL.Cust_Code
-                LEFT JOIN tspl_item_master 
-                    ON TSPL_DEMAND_BOOKING_DETAIL.item_code = tspl_item_master.item_code
-		            Left Join TSPL_ITEM_UOM_DETAIL On TSPL_ITEM_UOM_DETAIL.Item_Code=tspl_item_master.item_code And TSPL_ITEM_UOM_DETAIL.UOM_Code=TSPL_DEMAND_BOOKING_DETAIL.Unit_code
-                LEFT JOIN (
-                    SELECT 
-                        Conversion_Factor,
-                        Item_Code,
-                        Report_UOM,
-                        UOM_Code
-                    FROM TSPL_ITEM_UOM_DETAIL
-                    WHERE Report_UOM = 1
-                    ) AS ITEMDETAIL1 
-                    ON ITEMDETAIL1.Item_Code = TSPL_DEMAND_BOOKING_DETAIL.Item_Code
-                        where convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + txtfDate.Value + "',103) AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + txtToDate.Value + "',103) 
-                        ) AS SourceData
-                        PIVOT (
-                        SUM(Qty)
-                        FOR item_desc IN ([Tonned Milk 500 Ml], [Tonned Milk 1 LT], [Standard Milk 500 Ml], 
-                                              [Standard Milk 1 LTR], [GOLD Milk 500 Ml], [GOLD Milk 1 LT], 
-                                              [PLAIN CHHACH 1/2 LTR], [Tonned Milk 6 LT], [GOLD Milk 6 LT], [COW MILK 500 Ml])
-                        ) AS PivotTable"
-            ElseIf rbtnDetail.IsChecked Then
-                qry = "SELECT distinct
-                    [Date], [Both Code], [Customer Name], [Mobile No], [Route], [Route Name],[Item_Code],[Unit_code], [Shift],
-                    [Tonned Milk 500 Ml] AS [TM500], [Tonned Milk 1 LT] AS [TM1LT], [Standard Milk 500 Ml] AS [SM500],
-                    [Standard Milk 1 LTR] AS [SM1LT], [GOLD Milk 500 Ml] AS [GM500], [GOLD Milk 1 LT] AS [GM1LT],
-                    [PLAIN CHHACH 1/2 LTR] AS [CHHACH], [Tonned Milk 6 LT] AS [TM6LT], [GOLD Milk 6 LT] AS [GM6LT],
-                    [COW MILK 500 Ml] AS [COW500]
-                FROM (
-                    SELECT 
-                        CASE WHEN ITEMDETAIL1.Report_UOM=1 THEN ((Qty *isnull((TSPL_ITEM_UOM_DETAIL.Conversion_Factor),1))/ITEMDETAIL1.Conversion_Factor) end as [Quantity],
-                        CASE 
-                            WHEN ITEMDETAIL1.Report_UOM = 1 THEN ITEMDETAIL1.UOM_Code 
-                        END AS [UOM],
-                        CONVERT(VARCHAR, TSPL_DEMAND_BOOKING_MASTER.Document_Date, 103) AS [Date],
-                        TSPL_DEMAND_BOOKING_DETAIL.Cust_Code AS [Both Code],
-                        TSPL_CUSTOMER_MASTER.Customer_Name AS [Customer Name],
-                        TSPL_CUSTOMER_MASTER.Phone1 AS [Mobile No],
-                        TSPL_DEMAND_BOOKING_MASTER.Route_No AS [Route],
-                        TSPL_CUSTOMER_MASTER.Route_Desc AS [Route Name],
-                        TSPL_DEMAND_BOOKING_MASTER.ShiftType AS [Shift],
-		                tspl_item_master.Item_Code,
-                        tspl_item_master.Item_Desc,
-                        TSPL_DEMAND_BOOKING_DETAIL.Qty,
-		                TSPL_DEMAND_BOOKING_DETAIL.Unit_code
-                    FROM TSPL_DEMAND_BOOKING_MASTER
-                    LEFT JOIN TSPL_DEMAND_BOOKING_DETAIL 
-                        ON TSPL_DEMAND_BOOKING_DETAIL.Document_No = TSPL_DEMAND_BOOKING_MASTER.Document_No
-                    LEFT JOIN TSPL_CUSTOMER_MASTER 
-                        ON TSPL_CUSTOMER_MASTER.Cust_Code = TSPL_DEMAND_BOOKING_DETAIL.Cust_Code
-                    LEFT JOIN tspl_item_master 
-                        ON TSPL_DEMAND_BOOKING_DETAIL.item_code = tspl_item_master.item_code
-		                Left Join TSPL_ITEM_UOM_DETAIL On TSPL_ITEM_UOM_DETAIL.Item_Code=tspl_item_master.item_code And TSPL_ITEM_UOM_DETAIL.UOM_Code=TSPL_DEMAND_BOOKING_DETAIL.Unit_code
-                    LEFT JOIN (
+                                    qry = "SELECT 
+                        [Date], 
+                        [Booth Code], 
+                        MAX([Customer Name]) AS [Booth Name], 
+                        MAX([Mobile No]) AS [Mobile No], 
+                        MAX([Route]) AS [Route], 
+                        MAX([Route Name]) AS [Route Name], 
+                        [Shift],
+                        CAST(SUM([Tonned Milk 500 Ml]) AS DECIMAL(10, 2)) AS [TM500], 
+                        CAST(SUM([Tonned Milk 1 LTR]) AS DECIMAL(10, 2)) AS [TM1LT], 
+                        CAST(SUM([Standard Milk 500 Ml]) AS DECIMAL(10, 2)) AS [SM500],
+                        CAST(SUM([Standard Milk 1 LTR]) AS DECIMAL(10, 2)) AS [SM1LT], 
+                        CAST(SUM([GOLD Milk 500 Ml]) AS DECIMAL(10, 2)) AS [GM500], 
+                        CAST(SUM([GOLD Milk 1 LTR]) AS DECIMAL(10, 2)) AS [GM1LT],
+                        CAST(SUM([PLAIN CHHACH 1/2 LTR]) AS DECIMAL(10, 2)) AS [CHHACH], 
+                        CAST(SUM([TOND Milk 6Ltr]) AS DECIMAL(10, 2)) AS [TM6LT], 
+                        CAST(SUM([GOLD Milk 6Ltr]) AS DECIMAL(10, 2)) AS [GM6LT],
+                        CAST(SUM([COW MILK 500 Ml]) AS DECIMAL(10, 2)) AS [COW500]
+                    FROM (
                         SELECT 
-                            Conversion_Factor,
-                            Item_Code,
-                            Report_UOM,
-                            UOM_Code
-                        FROM TSPL_ITEM_UOM_DETAIL
-                        WHERE Report_UOM = 1
-                    ) AS ITEMDETAIL1 
-                        ON ITEMDETAIL1.Item_Code = TSPL_DEMAND_BOOKING_DETAIL.Item_Code
-                        where convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + txtfDate.Value + "',103) AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + txtToDate.Value + "',103)
+                            CASE 
+                                WHEN ITEMDETAIL1.Report_UOM = 1 THEN 
+                                    ((Qty * ISNULL(TSPL_ITEM_UOM_DETAIL.Conversion_Factor, 1)) / ITEMDETAIL1.Conversion_Factor)
+                            END AS [Quantity],
+                            CONVERT(VARCHAR, TSPL_DEMAND_BOOKING_MASTER.Document_Date, 103) AS [Date],
+                            TSPL_DEMAND_BOOKING_DETAIL.Cust_Code AS [Booth Code],
+                            TSPL_CUSTOMER_MASTER.Customer_Name AS [Customer Name],
+                            TSPL_CUSTOMER_MASTER.Phone1 AS [Mobile No],
+                            TSPL_ROUTE_MASTER.Route_No AS [Route],
+                            TSPL_CUSTOMER_MASTER.Route_Desc AS [Route Name],
+                            TSPL_DEMAND_BOOKING_MASTER.ShiftType AS [Shift],
+                            tspl_item_master.Item_Desc,
+                            TSPL_DEMAND_BOOKING_DETAIL.Qty
+                        FROM TSPL_DEMAND_BOOKING_MASTER
+                        LEFT OUTER JOIN TSPL_ROUTE_MASTER 
+                            ON TSPL_ROUTE_MASTER.Route_No = TSPL_DEMAND_BOOKING_MASTER.Route_No
+                        LEFT JOIN TSPL_DEMAND_BOOKING_DETAIL 
+                            ON TSPL_DEMAND_BOOKING_DETAIL.Document_No = TSPL_DEMAND_BOOKING_MASTER.Document_No
+                        LEFT JOIN TSPL_CUSTOMER_MASTER 
+                            ON TSPL_CUSTOMER_MASTER.Cust_Code = TSPL_DEMAND_BOOKING_DETAIL.Cust_Code
+                        LEFT JOIN tspl_item_master 
+                            ON TSPL_DEMAND_BOOKING_DETAIL.item_code = tspl_item_master.item_code
+                        LEFT JOIN TSPL_ITEM_UOM_DETAIL 
+                            ON TSPL_ITEM_UOM_DETAIL.Item_Code = tspl_item_master.item_code 
+                            AND TSPL_ITEM_UOM_DETAIL.UOM_Code = TSPL_DEMAND_BOOKING_DETAIL.Unit_code
+                        LEFT JOIN (
+                            SELECT 
+                                Conversion_Factor,
+                                Item_Code,
+                                Report_UOM,
+                                UOM_Code
+                            FROM TSPL_ITEM_UOM_DETAIL
+                            WHERE Report_UOM = 1
+                        ) AS ITEMDETAIL1 
+                            ON ITEMDETAIL1.Item_Code = TSPL_DEMAND_BOOKING_DETAIL.Item_Code
+                        where convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + txtfDate.Value + "',103) AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + txtToDate.Value + "',103) " + Whr + " 
                         ) AS SourceData
                         PIVOT (
-                            SUM(Qty)
-                            FOR item_desc IN ([Tonned Milk 500 Ml], [Tonned Milk 1 LT], [Standard Milk 500 Ml], 
-                                              [Standard Milk 1 LTR], [GOLD Milk 500 Ml], [GOLD Milk 1 LT], 
-                                              [PLAIN CHHACH 1/2 LTR], [Tonned Milk 6 LT], [GOLD Milk 6 LT], [COW MILK 500 Ml])
-                        ) AS PivotTable"
+                        SUM([Quantity])FOR item_desc IN ([Tonned Milk 500 Ml],[Tonned Milk 1 LTR],[Standard Milk 500 Ml],[Standard Milk 1 LTR], [GOLD Milk 500 Ml],[GOLD Milk 1 LTR], [PLAIN CHHACH 1/2 LTR], [TOND Milk 6Ltr],[GOLD Milk 6Ltr],[COW MILK 500 Ml])
+                        ) AS PivotTable
+                        GROUP BY 
+                        [Date], [Booth Code], [Shift] ORDER BY 
+                        CAST([Booth Code] AS INT)"
+            ElseIf rbtnSummary.IsChecked Then
+                qry = "SELECT 
+                        [Booth Code],
+                        MAX([Customer Name]) AS [Booth Name],
+                        MAX([Mobile No]) AS [Mobile No],
+                        MAX([Route]) AS [Route],
+                        MAX([Route Name]) AS [Route Name],
+                        CAST(SUM([Tonned Milk 500 Ml]) AS DECIMAL(10, 2)) AS [TM500], 
+                        CAST(SUM([Tonned Milk 1 LTR]) AS DECIMAL(10, 2)) AS [TM1LT], 
+                        CAST(SUM([Standard Milk 500 Ml]) AS DECIMAL(10, 2)) AS [SM500],
+                        CAST(SUM([Standard Milk 1 LTR]) AS DECIMAL(10, 2)) AS [SM1LT], 
+                        CAST(SUM([GOLD Milk 500 Ml]) AS DECIMAL(10, 2)) AS [GM500], 
+                        CAST(SUM([GOLD Milk 1 LTR]) AS DECIMAL(10, 2)) AS [GM1LT],
+                        CAST(SUM([PLAIN CHHACH 1/2 LTR]) AS DECIMAL(10, 2)) AS [CHHACH], 
+                        CAST(SUM([TOND Milk 6Ltr]) AS DECIMAL(10, 2)) AS [TM6LT], 
+                        CAST(SUM([GOLD Milk 6Ltr]) AS DECIMAL(10, 2)) AS [GM6LT],
+                        CAST(SUM([COW MILK 500 Ml]) AS DECIMAL(10, 2)) AS [COW500]
+                    FROM (
+                        SELECT 
+                            CASE 
+                                WHEN ITEMDETAIL1.Report_UOM = 1 THEN 
+                                    ((Qty * ISNULL(TSPL_ITEM_UOM_DETAIL.Conversion_Factor, 1)) / ITEMDETAIL1.Conversion_Factor)
+                            END AS [Quantity],
+                            TSPL_DEMAND_BOOKING_DETAIL.Cust_Code AS [Booth Code],
+                            TSPL_CUSTOMER_MASTER.Customer_Name AS [Customer Name],
+                            TSPL_CUSTOMER_MASTER.Phone1 AS [Mobile No],
+                            TSPL_ROUTE_MASTER.Route_No AS [Route],
+                            TSPL_CUSTOMER_MASTER.Route_Desc AS [Route Name],
+                            tspl_item_master.Item_Desc,
+                            TSPL_DEMAND_BOOKING_DETAIL.Qty,
+                            TSPL_DEMAND_BOOKING_DETAIL.Unit_code
+                        FROM TSPL_DEMAND_BOOKING_MASTER
+                        LEFT OUTER JOIN TSPL_ROUTE_MASTER 
+                            ON TSPL_ROUTE_MASTER.Route_No = TSPL_DEMAND_BOOKING_MASTER.Route_No
+                        LEFT JOIN TSPL_DEMAND_BOOKING_DETAIL 
+                            ON TSPL_DEMAND_BOOKING_DETAIL.Document_No = TSPL_DEMAND_BOOKING_MASTER.Document_No
+                        LEFT JOIN TSPL_CUSTOMER_MASTER 
+                            ON TSPL_CUSTOMER_MASTER.Cust_Code = TSPL_DEMAND_BOOKING_DETAIL.Cust_Code
+                        LEFT JOIN tspl_item_master 
+                            ON TSPL_DEMAND_BOOKING_DETAIL.item_code = tspl_item_master.item_code
+                        LEFT JOIN TSPL_ITEM_UOM_DETAIL 
+                            ON TSPL_ITEM_UOM_DETAIL.Item_Code = tspl_item_master.item_code 
+                            AND TSPL_ITEM_UOM_DETAIL.UOM_Code = TSPL_DEMAND_BOOKING_DETAIL.Unit_code
+                        LEFT JOIN (
+                            SELECT 
+                                Conversion_Factor,
+                                Item_Code,
+                                Report_UOM,
+                                UOM_Code
+                            FROM TSPL_ITEM_UOM_DETAIL
+                            WHERE Report_UOM = 1
+                        ) AS ITEMDETAIL1 
+                            ON ITEMDETAIL1.Item_Code = TSPL_DEMAND_BOOKING_DETAIL.Item_Code
+                        where convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + txtfDate.Value + "',103) AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + txtToDate.Value + "',103) " + Whr + " 
+                        ) AS SourceData
+                    PIVOT (
+                    SUM([Quantity])FOR item_desc IN ([Tonned Milk 500 Ml],[Tonned Milk 1 LTR],[Standard Milk 500 Ml],[Standard Milk 1 LTR], [GOLD Milk 500 Ml],[GOLD Milk 1 LTR], [PLAIN CHHACH 1/2 LTR], [TOND Milk 6Ltr],[GOLD Milk 6Ltr],[COW MILK 500 Ml])
+                    ) AS PivotTable
+                    GROUP BY 
+                    [Booth Code] ORDER BY 
+                    CAST([Booth Code] AS INT)"
             End If
             'qry += " ORDER BY TSPL_SD_SALE_INVOICE_HEAD.Document_Code"
             dt = clsDBFuncationality.GetDataTable(qry)
@@ -222,6 +253,11 @@ Public Class FrmBoothDemandReport
         End Try
     End Sub
 
+    Private Sub TxtRoute__My_Click(sender As Object, e As EventArgs) Handles TxtRoute._My_Click
+        Dim qry As String = "Select TSPL_ROUTE_MASTER.Route_No AS Code,TSPL_ROUTE_MASTER.Route_Desc as Name from TSPL_ROUTE_MASTER  where 1=1 "
+        TxtRoute.arrValueMember = clsCommon.ShowMultipleSelectForm("RouteMulSel", qry, "Code", "Name", TxtRoute.arrValueMember, TxtRoute.arrDispalyMember)
+    End Sub
+
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
         Close()
     End Sub
@@ -231,9 +267,9 @@ Public Class FrmBoothDemandReport
         txtToDate.Value = clsCommon.GETSERVERDATE()
         rbtnDetail.IsChecked = True
         rbtnSummary.IsChecked = False
-        rdbMilk.Checked = True
+        rdbMilk.Checked = False
         rdbProduct.Checked = False
-        rdbDemandBoth.Checked = False
+        rdbDemandBoth.Checked = True
     End Sub
 
     Private Sub rmenuExport_Click(sender As Object, e As EventArgs) Handles rmenuExport.Click
