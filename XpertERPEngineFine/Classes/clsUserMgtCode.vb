7ac8fc49ed89b806789c4100d292481c9157eaa6
MohdSuhail <mohd.suhail@tecxpert.in>
2025-01-07 11:46:02 +0530

    Add new report Tanker Approval Utilization report (Ashutosh Sir/JPR)

--- XpertERPEngineFine/Classes/clsUserMgtCode.vb
+++ XpertERPEngineFine/Classes/clsUserMgtCode.vb
@@ -2223,6 +2223,7 @@ Public Class clsUserMgtCode
     Public Const rptTankerStatusReport As String = "TNK_ST_RPT"
     Public Const rptTruckSheetReport As String = "TRU_SH_RPT"
     Public Const rptDailyQtyReport As String = "DAILY_QTY_R"
+    Public Const rptTankerUtilization As String = "TANKER_ULT_R"
     Public Const rptBMCCapacityUtilizationReport As String = "BMC_CP_UTLZ"
     Public Const rptBMCPerodicalReport As String = "BMC_PERO_R"
     Public Const rptTankerProfitLossReport As String = "TNR_P_L_R"
