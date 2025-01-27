69c7fa804c6631becaa9a5369f13ce946864fdbe
Shashi7433 <kalipujabelounja@gmail.com>
2025-01-27 11:37:01 +0530

    --New Screen Create - Booth Wise Bill Print [Prabhat Sir]

--- XpertERPDairySale/Report/rptBoothWiseBillReport.vb
+++ XpertERPDairySale/Report/rptBoothWiseBillReport.vb
@@ -65,6 +65,8 @@ Public Class rptBoothWiseBillReport
         ToDate.Value = clsCommon.GETSERVERDATE()
         fromDate.Value = ToDate.Value.AddMonths(-1)
         RadGroupBox1.Enabled = True
+        RadGroupBox4.Enabled = True
+        RadGroupBox2.Enabled = True
         txtRoute.Enabled = True
         TxtCustomer.Enabled = True
         GV1.DataSource = Nothing
@@ -74,8 +76,6 @@ Public Class rptBoothWiseBillReport
 
     Private Sub btnGO_Click(sender As Object, e As EventArgs) Handles btnGO.Click
         Try
-            'Dim qry As String = returnQry()
-            'qry += " ORDER BY TSPL_SD_SALE_INVOICE_HEAD.Document_Code"
             Dim Whr As String = ""
             Dim itemdesc As String = ""
             Dim Sumitemdesc As String = ""
@@ -86,14 +86,14 @@ Public Class rptBoothWiseBillReport
             Dim Qry As String = ""
             Dim strqry As String = ""
 
-            strqry += " SELECT distinct Short_Description + ' ' + '(' + case when isnull(it.Report_UOM,0) = 1 then  it.UOM_Code + ')' end AS Short_Description
+            strqry += " SELECT distinct Short_Description + ' ' + '(' + case when isnull(it.Report_UOM,0) = 1 then  it.UOM_Code + ')' end AS Short_Description, Sku_Seq
                          FROM TSPL_DEMAND_BOOKING_DETAIL 
                         Left Outer Join TSPL_DEMAND_BOOKING_MASTER On TSPL_DEMAND_BOOKING_MASTER.Document_No = TSPL_DEMAND_BOOKING_DETAIL.Document_No
                         Left Outer Join TSPL_CUSTOMER_MASTER On TSPL_CUSTOMER_MASTER.Cust_Code = TSPL_DEMAND_BOOKING_DETAIL.Cust_Code 
                         Left Outer Join TSPL_ITEM_MASTER On TSPL_ITEM_MASTER.Item_Code = TSPL_DEMAND_BOOKING_DETAIL.Item_Code 
 						left join ( select Item_Code,Report_UOM,UOM_Code from TSPL_ITEM_UOM_DETAIL where Report_UOM = 1 ) It on It.Item_Code =TSPL_ITEM_MASTER.Item_Code 
                         Left Outer Join TSPL_LOCATION_MASTER On TSPL_LOCATION_MASTER.Location_Code = TSPL_DEMAND_BOOKING_MASTER.location_code 
-                        left outer join TSPL_CUSTOMER_GROUP_MASTER on TSPL_CUSTOMER_GROUP_MASTER.Cust_Group_Code=TSPL_CUSTOMER_MASTER.Cust_Group_Code  where  convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + fromDate.Value + "',103) AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103) " + Whr + " order by Short_Description desc"
+                        left outer join TSPL_CUSTOMER_GROUP_MASTER on TSPL_CUSTOMER_GROUP_MASTER.Cust_Group_Code=TSPL_CUSTOMER_MASTER.Cust_Group_Code  where  convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + fromDate.Value + "',103) AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103) " + Whr + " order by Sku_Seq"
             dt1 = clsDBFuncationality.GetDataTable(strqry)
 
             If dt1 Is Nothing OrElse dt1.Rows.Count > 0 Then
@@ -258,23 +258,24 @@ Public Class rptBoothWiseBillReport
         End If
 
         If rbtnDetail.IsChecked Then
-            strqry = "SELECT distinct Short_Description + ' ' + '(' + case when isnull(it.Report_UOM,0) = 1 then  it.UOM_Code + ')' end AS Short_Description
+            strqry = "SELECT distinct Short_Description + ' ' + '(' + case when isnull(it.Report_UOM,0) = 1 then  it.UOM_Code + ')' end AS Short_Description, Sku_Seq
                          FROM TSPL_DEMAND_BOOKING_DETAIL 
                         Left Outer Join TSPL_DEMAND_BOOKING_MASTER On TSPL_DEMAND_BOOKING_MASTER.Document_No = TSPL_DEMAND_BOOKING_DETAIL.Document_No
                         Left Outer Join TSPL_CUSTOMER_MASTER On TSPL_CUSTOMER_MASTER.Cust_Code = TSPL_DEMAND_BOOKING_DETAIL.Cust_Code 
                         Left Outer Join TSPL_ITEM_MASTER On TSPL_ITEM_MASTER.Item_Code = TSPL_DEMAND_BOOKING_DETAIL.Item_Code 
 						left join ( select Item_Code,Report_UOM,UOM_Code from TSPL_ITEM_UOM_DETAIL where Report_UOM = 1 ) It on It.Item_Code =TSPL_ITEM_MASTER.Item_Code 
                         Left Outer Join TSPL_LOCATION_MASTER On TSPL_LOCATION_MASTER.Location_Code = TSPL_DEMAND_BOOKING_MASTER.location_code 
-                        left outer join TSPL_CUSTOMER_GROUP_MASTER on TSPL_CUSTOMER_GROUP_MASTER.Cust_Group_Code=TSPL_CUSTOMER_MASTER.Cust_Group_Code  where  convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + fromDate.Value + "',103) AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103) " + Whr + " order by Short_Description desc"
+                        left outer join TSPL_CUSTOMER_GROUP_MASTER on TSPL_CUSTOMER_GROUP_MASTER.Cust_Group_Code=TSPL_CUSTOMER_MASTER.Cust_Group_Code  where  convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + fromDate.Value + "',103) AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103) " + Whr + " order by Sku_Seq"
+
         ElseIf rbtnSummary.IsChecked Then
-            strqry = " SELECT distinct Short_Description
+            strqry = " SELECT distinct Short_Description, Sku_Seq
                          FROM TSPL_DEMAND_BOOKING_DETAIL 
                         Left Outer Join TSPL_DEMAND_BOOKING_MASTER On TSPL_DEMAND_BOOKING_MASTER.Document_No = TSPL_DEMAND_BOOKING_DETAIL.Document_No
                         Left Outer Join TSPL_CUSTOMER_MASTER On TSPL_CUSTOMER_MASTER.Cust_Code = TSPL_DEMAND_BOOKING_DETAIL.Cust_Code 
                         Left Outer Join TSPL_ITEM_MASTER On TSPL_ITEM_MASTER.Item_Code = TSPL_DEMAND_BOOKING_DETAIL.Item_Code 
 						left join ( select Item_Code,Report_UOM,UOM_Code from TSPL_ITEM_UOM_DETAIL where Report_UOM = 1 ) It on It.Item_Code =TSPL_ITEM_MASTER.Item_Code 
                         Left Outer Join TSPL_LOCATION_MASTER On TSPL_LOCATION_MASTER.Location_Code = TSPL_DEMAND_BOOKING_MASTER.location_code 
-                        left outer join TSPL_CUSTOMER_GROUP_MASTER on TSPL_CUSTOMER_GROUP_MASTER.Cust_Group_Code=TSPL_CUSTOMER_MASTER.Cust_Group_Code  where  convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + fromDate.Value + "',103) AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103) " + whrh + " order by Short_Description desc"
+                        left outer join TSPL_CUSTOMER_GROUP_MASTER on TSPL_CUSTOMER_GROUP_MASTER.Cust_Group_Code=TSPL_CUSTOMER_MASTER.Cust_Group_Code  where  convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + fromDate.Value + "',103) AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103) " + whrh + " order by Sku_Seq"
         End If
 
         '  strqry += " SELECT distinct Short_Description + ' ' + '(' + case when isnull(it.Report_UOM,0) = 1 then  it.UOM_Code + ')' end AS Short_Description
@@ -298,9 +299,6 @@ Public Class rptBoothWiseBillReport
                         itemdesc += ",[" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]"
                         Sumitemdesc += ", CAST(SUM([" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]) AS DECIMAL(10, 2)) As [" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]"
                     End If
-
-                    'itemdesc += ",[" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]"
-                    'Sumitemdesc += ", ISNULL(CAST(SUM([" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]) AS DECIMAL(10, 2)), 0) As [" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]"
                 Else
                     If rbtnDetail.IsChecked Then
                         itemdesc = "[" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]"
@@ -309,9 +307,6 @@ Public Class rptBoothWiseBillReport
                         itemdesc = "[" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]"
                         Sumitemdesc += " CAST(SUM([" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]) AS DECIMAL(10, 2)) As [" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]"
                     End If
-
-                    ' itemdesc = "[" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]"
-                    ' Sumitemdesc += " ISNULL(CAST(SUM([" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]) AS DECIMAL(10, 2)), 0) As [" + clsCommon.myCstr(dt1.Rows(kk)("Short_Description")) + "]"
                 End If
             Next
         Else
@@ -361,12 +356,13 @@ Public Class rptBoothWiseBillReport
             AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103)" + Whr + " ORDER BY CAST(TSPL_DEMAND_BOOKING_DETAIL.Cust_Code AS INT) "
 
         ElseIf rbtnSummary.IsChecked Then
-            qry = "SELECT ROW_NUMBER() OVER (PARTITION BY TSPL_DEMAND_BOOKING_DETAIL.Cust_Code ORDER BY TSPL_DEMAND_BOOKING_MASTER.Document_Date) AS Sl_No, '" + fromDate.Value + "' As 'From_Date',Convert(Varchar(10),'" + ToDate.Value + "') As 'To_Date', MAX(CAST(Logo_Img AS VARBINARY(MAX))) AS Logo_Img, max(TSPL_COMPANY_MASTER.GSTReg_No) As SellerGST, max (TSPL_COMPANY_MASTER.Pan_No) as [Comp Pan], max (TSPL_COMPANY_MASTER.Comp_Name) as Comp_Name, max (TSPL_COMPANY_MASTER.City_Code) as City_Code, max (TSPL_COMPANY_MASTER.comp_code) as comp_code, max (TSPL_COMPANY_MASTER.add1) as Add1, max (TSPL_COMPANY_MASTER.add2) as add2, max (TSPL_COMPANY_MASTER.add3)as add3, max (TSPL_COMPANY_MASTER.Fax) as Fax, max(TSPL_COMPANY_MASTER.Email) as Email, max (TSPL_COMPANY_MASTER.Phone1) as Phone1, max (TSPL_COMPANY_MASTER.Phone2) as Phone2, max(TSPL_CUSTOMER_MASTER.add1) as CusAdd1, max(TSPL_CUSTOMER_MASTER.Add2) as CusAdd2, max(TSPL_CUSTOMER_MASTER.Add3) as CusAdd3, max (TSPL_CUSTOMER_MASTER.State) as State, max (TSPL_CUSTOMER_MASTER.Zone_Code) as [Zone_Code], max (TSPL_CUSTOMER_MASTER.PAN) as [Cust Pan], max (TSPL_CUSTOMER_MASTER.GSTNO) as Cust_GST_NO, max (TSPL_CUSTOMER_MASTER.Customer_Name) AS [Customer Name], max (TSPL_CUSTOMER_MASTER.Phone1) AS [Mobile No], max(TSPL_LOCATION_MASTER.Location_Desc) as Location,  max(TSPL_ROUTE_MASTER.Route_No) AS [Route], max (TSPL_CUSTOMER_MASTER.Route_Desc) AS [Route Name],  max(CONVERT(VARCHAR, TSPL_DEMAND_BOOKING_MASTER.Document_Date, 103)) AS [Date],  (CAST(TSPL_DEMAND_BOOKING_DETAIL.Cust_Code AS INT)) As [Booth Code], 
+            qry = "select *,(ItemNetAmount/Quantity_Pouch) as Rate_Item from ( SELECT ROW_NUMBER() OVER (PARTITION BY TSPL_DEMAND_BOOKING_DETAIL.Cust_Code ORDER BY MAX(TSPL_DEMAND_BOOKING_MASTER.Document_Date)) AS Sl_No, 
+'" + fromDate.Value + "' As 'From_Date',Convert(Varchar(10),'" + ToDate.Value + "') As 'To_Date', MAX(CAST(Logo_Img AS VARBINARY(MAX))) AS Logo_Img, max(TSPL_COMPANY_MASTER.GSTReg_No) As SellerGST, max (TSPL_COMPANY_MASTER.Pan_No) as [Comp Pan], max (TSPL_COMPANY_MASTER.Comp_Name) as Comp_Name, max (TSPL_COMPANY_MASTER.City_Code) as City_Code, max (TSPL_COMPANY_MASTER.comp_code) as comp_code, max (TSPL_COMPANY_MASTER.add1) as Add1, max (TSPL_COMPANY_MASTER.add2) as add2, max (TSPL_COMPANY_MASTER.add3)as add3, max (TSPL_COMPANY_MASTER.Fax) as Fax, max(TSPL_COMPANY_MASTER.Email) as Email, max (TSPL_COMPANY_MASTER.Phone1) as Phone1, max (TSPL_COMPANY_MASTER.Phone2) as Phone2, max(TSPL_CUSTOMER_MASTER.add1) as CusAdd1, max(TSPL_CUSTOMER_MASTER.Add2) as CusAdd2, max(TSPL_CUSTOMER_MASTER.Add3) as CusAdd3, max (TSPL_CUSTOMER_MASTER.State) as State, max (TSPL_CUSTOMER_MASTER.Zone_Code) as [Zone_Code], max (TSPL_CUSTOMER_MASTER.PAN) as [Cust Pan], max (TSPL_CUSTOMER_MASTER.GSTNO) as Cust_GST_NO, max (TSPL_CUSTOMER_MASTER.Customer_Name) AS [Customer Name], max (TSPL_CUSTOMER_MASTER.Phone1) AS [Mobile No], max(TSPL_LOCATION_MASTER.Location_Desc) as Location,  max(TSPL_ROUTE_MASTER.Route_No) AS [Route], max (TSPL_CUSTOMER_MASTER.Route_Desc) AS [Route Name],  max(CONVERT(date, TSPL_DEMAND_BOOKING_MASTER.Document_Date, 103)) AS [Date], MAX(Sku_Seq) AS Sku_Seq,
+            (CAST(TSPL_DEMAND_BOOKING_DETAIL.Cust_Code AS INT)) As [Booth Code], 	
 			MAX(CASE WHEN TSPL_DEMAND_BOOKING_MASTER.ShiftType = 'Morning' THEN 1 WHEN TSPL_DEMAND_BOOKING_MASTER.ShiftType = 'Evening' THEN 2 END) AS ShiftType,
 			MAX(CASE WHEN TSPL_DEMAND_BOOKING_MASTER.ShiftType = 'Morning' THEN 'M' WHEN TSPL_DEMAND_BOOKING_MASTER.ShiftType = 'Evening' THEN 'E' END) AS [Shift],
-			max (TSPL_ITEM_MASTER.Short_Description) AS Short_Description, max (HSN_Code)HSN_Code,TSPL_DEMAND_BOOKING_DETAIL.Item_Code, max (TSPL_DEMAND_BOOKING_DETAIL.Qty) as Qty, 
-			MAX(CASE WHEN ITEMDETAIL1.Report_UOM = 1 THEN ((Qty * ISNULL(TSPL_ITEM_UOM_DETAIL.Conversion_Factor, 1)) / ITEMDETAIL1.Conversion_Factor)END) AS [Quantity_Pouch],
-			-- MAX(CASE WHEN ITEMDETAIL1.Report_UOM = 1 THEN ((TSPL_DEMAND_BOOKING_DETAIL.Item_Rate) / ITEMDETAIL1.Conversion_Factor )END )  as  Rate_Item,
+			max (TSPL_ITEM_MASTER.Short_Description) AS Short_Description, max (HSN_Code)HSN_Code,TSPL_DEMAND_BOOKING_DETAIL.Item_Code, sum (TSPL_DEMAND_BOOKING_DETAIL.Qty) as Qty, 
+			sum(CASE WHEN ITEMDETAIL1.Report_UOM = 1 THEN ((Qty * ISNULL(TSPL_ITEM_UOM_DETAIL.Conversion_Factor, 1)) / ITEMDETAIL1.Conversion_Factor)END) AS [Quantity_Pouch],
 			sum(ItemNetAmount)as ItemNetAmount, max(TSPL_DEMAND_BOOKING_DETAIL.Item_Rate)Item_Rate,
 			max (case when isnull(ITEMDETAIL1.Report_UOM,0) = 1 then  ITEMDETAIL1.UOM_Code end) as [Report_UOM] FROM TSPL_DEMAND_BOOKING_MASTER
             LEFT OUTER JOIN TSPL_ROUTE_MASTER ON TSPL_ROUTE_MASTER.Route_No = TSPL_DEMAND_BOOKING_MASTER.Route_No
@@ -378,22 +374,20 @@ Public Class rptBoothWiseBillReport
             left outer Join TSPL_COMPANY_MASTER On TSPL_COMPANY_MASTER.Comp_Code =TSPL_DEMAND_BOOKING_MASTER.Comp_Code          
             iNNER  JOIN (SELECT Conversion_Factor, Item_Code, Report_UOM, UOM_Code FROM TSPL_ITEM_UOM_DETAIL WHERE Report_UOM = 1 ) AS ITEMDETAIL1 
             ON ITEMDETAIL1.Item_Code = TSPL_DEMAND_BOOKING_DETAIL.Item_Code	 where convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + fromDate.Value + "',103) 
-            AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103)" + Whr + " Group by TSPL_DEMAND_BOOKING_MASTER.Document_Date,TSPL_DEMAND_BOOKING_DETAIL.Cust_Code,TSPL_DEMAND_BOOKING_DETAIL.Item_Code ORDER BY TSPL_DEMAND_BOOKING_MASTER.Document_Date"
+            AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103)" + Whr + "  Group by TSPL_DEMAND_BOOKING_DETAIL.Cust_Code,TSPL_DEMAND_BOOKING_DETAIL.Item_Code, Item_Rate)xx order by Sku_Seq"
         End If
-
         Return qry
     End Function
 
     Private Sub PrintBoothBill_Click(sender As Object, e As EventArgs) Handles PrintBoothBill.Click
         Try
             Dim frmCRV As New frmCrystalReportViewer()
-
             Dim dt As DataTable = Nothing
             Dim qry As String = returnQry()
             dt = clsDBFuncationality.GetDataTable(qry)
             Dim Whr As String = ""
             If rdbMilk.IsChecked Then
-                Whr += " and TSPL_DEMAND_BOOKING_MASTER.ItemType='Fresh' "
+                Whr += " And TSPL_DEMAND_BOOKING_MASTER.ItemType ='Fresh' "
             ElseIf rdbProduct.IsChecked Then
                 Whr += " and TSPL_DEMAND_BOOKING_MASTER.ItemType='Product' "
             ElseIf rdbBoth.IsChecked Then
@@ -416,9 +410,9 @@ Public Class rptBoothWiseBillReport
 
             If dt IsNot Nothing AndAlso dt.Rows.Count > 0 Then
                 If rbtnDetail.IsChecked Then
-                    Dim query As String = "SELECT (CONVERT(DECIMAL(18, 2), (Amount / ((QtyLtr * ConversionFactor) / CF)))) AS RateLtr, (Sku_Seq) AS Sku_Seq, (Item_Desc) AS Item_Desc,  (Amount) AS Amount, (QtyLtr) AS QtyLtr, 
+                    Dim query As String = "SELECT (Amount / NULLIF(QtyLtr, 0)) AS Rate_Item, (Sku_Seq) AS Sku_Seq, (Item_Desc) AS Item_Desc,  (Amount) AS Amount, (QtyLtr) AS QtyLtr, 
                     (ConversionFactor) AS ConversionFactor,  (CF) AS CF,  (Report_UOM) AS Report_UOM,(Route_No)Route_No,(Cust_Code)as [Booth Code] from (SELECT MAX(Sku_Seq) AS Sku_Seq, MAX(Short_Description) AS Item_Desc,  SUM(ItemNetAmount) AS Amount, SUM(TotalLtr_ItemWise) AS QtyLtr, 
-                    MAX(ConversionFactor) AS ConversionFactor,  MAX(CF) AS CF,  MAX(Report_UOM) AS Report_UOM,(Route_No)Route_No,(Cust_Code)Cust_Code FROM (SELECT TSPL_DEMAND_BOOKING_MASTER.Route_No,
+                    MAX(ConversionFactor) AS ConversionFactor,  MAX(CF) AS CF,  MAX(Report_UOM) AS Report_UOM,max(Route_No)Route_No,(Cust_Code)Cust_Code FROM (SELECT TSPL_DEMAND_BOOKING_MASTER.Route_No,
                     CONVERT(DATE, TSPL_DEMAND_BOOKING_MASTER.Document_Date, 103) AS Document_Date,TSPL_DEMAND_BOOKING_DETAIL.Cust_Code,TSPL_ITEM_MASTER.Item_Code,
                     TSPL_ITEM_MASTER.Sku_Seq, TSPL_ITEM_MASTER.Short_Description, TSPL_DEMAND_BOOKING_DETAIL.Qty, TSPL_DEMAND_BOOKING_DETAIL.ItemNetAmount, TSPL_ITEM_MASTER.Rate, 
                     TSPL_DEMAND_BOOKING_DETAIL.TotalLtr_ItemWise, TSPL_ITEM_UOM_DETAIL.Conversion_Factor AS ConversionFactor, ITEMDETAIL1.Conversion_Factor AS CF, ITEMDETAIL1.UOM_Code AS Report_UOM FROM TSPL_DEMAND_BOOKING_DETAIL 
@@ -428,11 +422,11 @@ Public Class rptBoothWiseBillReport
                     AND TSPL_ITEM_UOM_DETAIL.UOM_Code = TSPL_DEMAND_BOOKING_DETAIL.Unit_Code 
                     LEFT JOIN (SELECT Conversion_Factor, Item_Code, Report_UOM, UOM_Code FROM TSPL_ITEM_UOM_DETAIL WHERE Report_UOM = 1) AS ITEMDETAIL1 ON ITEMDETAIL1.Item_Code = TSPL_DEMAND_BOOKING_DETAIL.Item_Code WHERE TSPL_ITEM_MASTER.Item_Type = 'F' 
                     AND TSPL_ITEM_MASTER.Is_FreshItem = 1 AND TSPL_DEMAND_BOOKING_MASTER.Posted = 1  And convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date ,103)>=convert(date,'" + fromDate.Value + "',103)
-                    AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103) " + Whr + " )xx group by Route_No, Cust_Code, Item_Code )xxx order by Sku_Seq"
+                    AND convert(date,TSPL_DEMAND_BOOKING_MASTER.Document_Date,103)<=convert(date,'" + ToDate.Value + "',103) " + Whr + " )xx group by Cust_Code, Item_Code )xxx order by Sku_Seq"
 
                     Dim dt2 As DataTable = clsDBFuncationality.GetDataTable(query)
-
-                    frmCRV.funsubreportWithdt(CrystalReportFolder.KwalitySalesReport, dt, dt2, "crptBoothWiseBillDetail", "Booth Wise Bill Detail", "SubReportInvoiceDetail")
+                    Throw New Exception("The Print Button does not function for detailed printing.")
+                    ' frmCRV.funsubreportWithdt(CrystalReportFolder.KwalitySalesReport, dt, dt2, "crptBoothWiseBillDetail", "Booth Wise Bill Detail", "SubReportInvoiceDetail")
                 Else
                     frmCRV.funsubreportWithdt(CrystalReportFolder.KwalitySalesReport, dt, clsERPFuncationality.CompanyAddresShowinFooter(), "crptBoothWiseBillSummary", "Booth Wise Bill Summary Report", Nothing, "crptBoothWiseBillSummary.rpt", clsERPFuncationality.CompanyAddresInvoiceHeader())
                 End If
@@ -498,7 +492,6 @@ Public Class rptBoothWiseBillReport
         Catch ex As Exception
             common.clsCommon.MyMessageBoxShow(ex.Message, "Error", MessageBoxButtons.OK, RadMessageIcon.Error)
         End Try
-
     End Sub
 
 End Class
