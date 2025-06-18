30aef53cb66b89f23624aeec84c679ddabda6133
balwinder.singh@tecxpert.in <balwinder.singh@tecxpert.in>
2025-06-18 10:20:30 +0530

    Exe version 7.1.4.66

--- XpertERPService/My Project/SharedAssemblyInfo.vb
+++ XpertERPService/My Project/SharedAssemblyInfo.vb
@@ -7,7 +7,7 @@ Imports System.Runtime.InteropServices
 ' associated with an assembly.
 ' Review the values of the assembly attributes 
 <Assembly: AssemblyTitle("XPERT ERP")>
-<Assembly: AssemblyDescription("17-06-2025")>
+<Assembly: AssemblyDescription("18-06-2025")>
 <Assembly: AssemblyCompany("Tecxpert Software Private Limited")>
 <Assembly: AssemblyCopyright("Copyright © Tecxpert 2008-2025")>
 <Assembly: AssemblyTrademark("")>
