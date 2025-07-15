6c2cbf65a34a30974cdc7e3b43122aa002e74d8b
varsha kumari <51097varsha@gmail.com>
2025-07-15 16:26:01 +0530

    Added New Report LastDcsCollection/Item

--- XpertERPBulkProcurement/Milk Procurement/Report/FrmLastDCSCollectionItem.vb
+++ XpertERPBulkProcurement/Milk Procurement/Report/FrmLastDCSCollectionItem.vb
@@ -11,13 +11,15 @@ Public Class FrmLastDCSCollectionItem
     End Sub
     Public Sub LoadData()
         Try
+            txtMultBmc.Enabled = False
+            txtMultDCS.Enabled = False
+            txtDate.Enabled = False
             Dim dt As New DataTable
             Dim strQry As String = Nothing
             Dim whrcls As String = ""
             Dim whrclsDCSSALE As String = ""
-
-            whrcls = "  convert(date,DOC_DATE,103) <convert(date,'" + txtDate.Value + "' ,103) "
-            whrclsDCSSALE = "convert(date,TSPL_SD_SALE_INVOICE_HEAD.Document_DATE,103) <convert(date,'" + txtDate.Value + "' ,103) "
+            whrcls = "  convert(date,TSPL_MILK_SRN_HEAD.DOC_DATE,103) <=convert(date,'" + txtDate.Value + "' ,103) "
+            whrclsDCSSALE = "convert(date,TSPL_SD_SALE_INVOICE_HEAD.Document_DATE,103) <=convert(date,'" + txtDate.Value + "' ,103) "
             If txtMultBmc.arrValueMember IsNot Nothing AndAlso txtMultBmc.arrValueMember.Count > 0 Then
                 whrcls += "  and TSPL_MCC_MASTER.MCC_Code in (" + clsCommon.GetMulcallString(txtMultBmc.arrValueMember) + ")  "
                 whrclsDCSSALE += "  and TSPL_MCC_MASTER.MCC_Code in (" + clsCommon.GetMulcallString(txtMultBmc.arrValueMember) + ")  "
@@ -28,24 +30,52 @@ Public Class FrmLastDCSCollectionItem
             End If
 
             If rbtnMilkSrn.Checked Then
-                strQry = " select TSPL_MCC_MASTER.MCC_Code as [BMC Code],MCC_NAME as[BMC NAME],TSPL_MCC_MASTER.Mcc_Code_VLC_Uploader as [ BMC UploaderNo],
-TSPL_VLC_MASTER_HEAD.VLC_Code as [DCS Code],TSPL_VLC_MASTER_HEAD.VLC_Code_VLC_Uploader as [DCS Uploader Code] ,TSPL_VLC_MASTER_HEAD.VLC_Name as [DCS Name],
-FORMAT(TSPL_MILK_SRN_HEAD.DOC_DATE, 'dd/MM/yyyy') as [Date],SHIFT as [Shift],TSPL_MILK_SRN_detail.ACC_Qty as [QTY],TSPL_MILK_SRN_detail.FAT_PER as [Fat Per],TSPL_MILK_SRN_detail.SNF_PER as [SNF Per],TSPL_MILK_SRN_detail.FAT_KG as [Fat Kg],TSPL_MILK_SRN_detail.SNF_KG as [SNF KG] 
-from  TSPL_MILK_SRN_HEAD
-LEFT OUTER JOIN  TSPL_MILK_SRN_detail on TSPL_MILK_SRN_detail.DOC_CODE=TSPL_MILK_SRN_HEAD.DOC_CODE
+                strQry = " select * from (
+ select row_number() over (Partition by [DCS Code] order by [Date] desc,[Shift] desc) as sno ,xx.* from (
+ select  
+ TSPL_MCC_MASTER.MCC_Code as [BMC Code],
+ (MCC_NAME) as[BMC NAME],
+ (TSPL_MCC_MASTER.Mcc_Code_VLC_Uploader) as Mcc_Code_VLC_Uploader,
+(TSPL_VLC_MASTER_HEAD.VLC_Code) as [DCS Code],	
+(TSPL_VLC_MASTER_HEAD.VLC_Code_VLC_Uploader) as VLC_Code_VLC_Uploader ,
+(TSPL_VLC_MASTER_HEAD.VLC_Name) as [DCS Name],
+cast(TSPL_MILK_SRN_HEAD.DOC_DATE as date) as [Date],
+SHIFT as [Shift],
+(TSPL_MILK_SRN_detail.ACC_Qty) as qty,
+(TSPL_MILK_SRN_detail.FAT_PER) as [Fat Per],
+(TSPL_MILK_SRN_detail.SNF_PER) as [SNF Per],
+(TSPL_MILK_SRN_detail.FAT_KG) as [Fat Kg],
+(TSPL_MILK_SRN_detail.SNF_KG) as [SNF KG] 
+from  TSPL_MILK_SRN_detail 
+LEFT OUTER JOIN TSPL_MILK_SRN_HEAD on TSPL_MILK_SRN_detail.DOC_CODE=TSPL_MILK_SRN_HEAD.DOC_CODE
 LEFT OUTER JOIN TSPL_VLC_MASTER_HEAD ON TSPL_VLC_MASTER_HEAD.VLC_Code=TSPL_MILK_SRN_HEAD.VLC_CODE
-LEFT OUTER JOIN TSPL_MCC_MASTER ON TSPL_MCC_MASTER.MCC_Code=TSPL_MILK_SRN_HEAD.MCC_CODE where " & whrcls & ""
+LEFT OUTER JOIN TSPL_MCC_MASTER ON TSPL_MCC_MASTER.MCC_Code=TSPL_MILK_SRN_HEAD.MCC_CODE 
+
+where " & whrcls & " )xx
+)xxx where sno=1"
             Else
-                strQry = "SELECT TSPL_VLC_MASTER_HEAD.VLC_Code as [DCS Code],TSPL_VLC_MASTER_HEAD.VLC_Code_VLC_Uploader as [DCS Uploader Code] , TSPL_VLC_MASTER_HEAD.VSP_Code as [DCS Code],
-Item_Code as [Item Code],qty as [Qty],TSPL_SD_SALE_INVOICE_HEAD.Document_Date as [Date] 
-FROM TSPL_SD_SALE_INVOICE_DETAIL
+                strQry = "select * from (
+ select row_number() over (Partition by [DCS Code] order by [Date] desc) as sno ,xx.* from 
+ (
+ SELECT TSPL_MCC_MASTER.MCC_Code as [BMC Code],
+(MCC_NAME) as[BMC NAME],
+(TSPL_MCC_MASTER.Mcc_Code_VLC_Uploader) as Mcc_Code_VLC_Uploader,
+TSPL_VLC_MASTER_HEAD.VLC_Code as [DCS Code],
+(TSPL_VLC_MASTER_HEAD.VLC_Name) as [DCS Name],
+(TSPL_VLC_MASTER_HEAD.VLC_Code_VLC_Uploader) as VLC_Code_VLC_Uploader ,
+(Item_Code) as [Item Code],
+(qty) as Qty,
+FORMAT(TSPL_SD_SALE_INVOICE_HEAD.Document_Date, 'dd/MM/yyyy') as [Date] FROM TSPL_SD_SALE_INVOICE_DETAIL
+
 LEFT OUTER JOIN  TSPL_SD_SALE_INVOICE_HEAD on TSPL_SD_SALE_INVOICE_HEAD.Document_Code=TSPL_SD_SALE_INVOICE_DETAIL.Document_Code
 left outer join TSPL_CUSTOMER_MASTER on TSPL_CUSTOMER_MASTER.Cust_Code = TSPL_SD_SALE_INVOICE_HEAD.Customer_Code 
  left join TSPL_CUSTOMER_VENDOR_MAPPING on TSPL_CUSTOMER_VENDOR_MAPPING.Cust_Code=TSPL_CUSTOMER_MASTER.Cust_Code 
  left join TSPL_VENDOR_MASTER on TSPL_VENDOR_MASTER.Vendor_Code=TSPL_CUSTOMER_VENDOR_MAPPING.Vendor_Code
 left join TSPL_VLC_MASTER_HEAD on TSPL_VENDOR_MASTER.Vendor_Code=TSPL_VLC_MASTER_HEAD.VSP_Code 
   Left Outer Join TSPL_MCC_MASTER ON TSPL_MCC_MASTER.MCC_Code=TSPL_VLC_MASTER_HEAD.MCC
-where Trans_Type='MCC'  and  " & whrclsDCSSALE & ""
+
+where  " & whrclsDCSSALE & "   )xx
+)xxx where sno=1  AND ISNULL(LTRIM(RTRIM([DCS Code])), '') <> ''"
             End If
             dt = clsDBFuncationality.GetDataTable(strQry)
             Gv1.DataSource = Nothing
@@ -72,17 +102,56 @@ where Trans_Type='MCC'  and  " & whrclsDCSSALE & ""
         End Try
     End Sub
     Sub FormatGrid()
-        Dim summaryItem As New GridViewSummaryItem()
-        Gv1.TableElement.TableHeaderHeight = 25
-        Gv1.MasterTemplate.ShowRowHeaderColumn = True
+
+        Gv1.AutoExpandGroups = False
+        Gv1.ShowGroupPanel = False
+        Gv1.ShowRowHeaderColumn = False
+        Gv1.AllowAddNewRow = False
+        Gv1.AllowDeleteRow = False
+        Gv1.EnableFiltering = True
+        Gv1.ShowFilteringRow = True
+
+
         For ii As Integer = 0 To Gv1.Columns.Count - 1
             Gv1.Columns(ii).ReadOnly = True
-            Gv1.Columns(ii).IsVisible = True
-            If rbtnMilkSrn.Checked And rbtnDCSSaleReturn.Checked Then
-                Gv1.Columns("BMC UploaderNo").IsVisible = False
-                Gv1.Columns("DCS Uploader Code").IsVisible = False
-            End If
+            Gv1.Columns(ii).BestFit()
         Next
+        If rbtnMilkSrn.Checked OrElse rbtnDCSSaleReturn.Checked Then
+            Gv1.Columns("Mcc_Code_VLC_Uploader").HeaderText = "BMC UploaderNo"
+            Gv1.Columns("Mcc_Code_VLC_Uploader").IsVisible = False
+            Gv1.Columns("VLC_Code_VLC_Uploader").HeaderText = "DCS Uploader Code"
+            Gv1.Columns("VLC_Code_VLC_Uploader").IsVisible = False
+            Gv1.Columns("sno").IsVisible = False
+
+        End If
+        Gv1.Columns("Qty").HeaderText = "Qty"
+
+
+
+        Dim summaryRowItem As New GridViewSummaryRowItem()
+        Dim intCount As Integer = 0
+        Dim qty As New GridViewSummaryItem("Qty", "{0:F3}", GridAggregateFunction.Sum)
+        summaryRowItem.Add(qty)
+
+        'Dim FATKG As New GridViewSummaryItem("FAT KG", "{0:F2}", GridAggregateFunction.Sum)
+        'summaryRowItem.Add(FATKG)
+        'Dim SNFKG As New GridViewSummaryItem("SNF KG", "{0:F2}", GridAggregateFunction.Sum)
+        'summaryRowItem.Add(SNFKG)
+        Gv1.ShowGroupPanel = True
+        Gv1.MasterTemplate.AutoExpandGroups = True
+        Gv1.MasterTemplate.SummaryRowsBottom.Add(summaryRowItem)
+        Gv1.MasterView.SummaryRows(0).PinPosition = PinnedRowPosition.Bottom
+        'Dim summaryItem As New GridViewSummaryItem()
+        'Gv1.TableElement.TableHeaderHeight = 25
+        'Gv1.MasterTemplate.ShowRowHeaderColumn = True
+        'For ii As Integer = 0 To Gv1.Columns.Count - 1
+        '    Gv1.Columns(ii).ReadOnly = True
+        '    Gv1.Columns(ii).IsVisible = True
+        '    If rbtnMilkSrn.Checked OrElse rbtnDCSSaleReturn.Checked Then
+        '        Gv1.Columns("BMC UploaderNo").IsVisible = False
+        '        Gv1.Columns("DCS Uploader Code").IsVisible = False
+        '    End If
+        'Next
     End Sub
 
 
@@ -96,7 +165,9 @@ where Trans_Type='MCC'  and  " & whrclsDCSSALE & ""
     End Sub
 
     Private Sub txtMultDCS__My_Click(sender As Object, e As EventArgs) Handles txtMultDCS._My_Click
-        Dim qry As String = " select TSPL_VLC_MASTER_HEAD.VLC_Code as [Code],TSPL_VLC_MASTER_HEAD.VLC_Code_VLC_Uploader as [DCS Uploader Code] ,TSPL_VLC_MASTER_HEAD.VLC_Name as [DCS Name] from TSPL_VLC_MASTER_HEAD"
+        Dim qry As String = " select TSPL_VLC_MASTER_HEAD.VLC_Code as [Code],TSPL_VLC_MASTER_HEAD.VLC_Code_VLC_Uploader as [DCS Uploader Code] ,TSPL_VLC_MASTER_HEAD.VLC_Name as [DCS Name] from TSPL_VLC_MASTER_HEAD
+left outer join TSPL_MCC_MASTER on TSPL_MCC_MASTER.MCC_Code=TSPL_VLC_MASTER_HEAD.MCC
+where  TSPL_MCC_MASTER.MCC_Code in (" + clsCommon.GetMulcallString(txtMultBmc.arrValueMember) + ") "
         txtMultDCS.arrValueMember = clsCommon.ShowMultipleSelectForm("VSPMulSelect", qry, "Code", "", txtMultDCS.arrValueMember, txtMultDCS.arrDispalyMember)
     End Sub
 
@@ -105,9 +176,11 @@ where Trans_Type='MCC'  and  " & whrclsDCSSALE & ""
     End Sub
     Private Sub Reset()
         'txtMultBmc.Enabled=True
-        txtMultBmc.arrValueMember = Nothing
-        txtMultDCS.arrValueMember = Nothing
+        txtMultBmc.Enabled = True
+        txtMultDCS.Enabled = True
         txtDate.Enabled = True
+        'txtMultBmc.arrValueMember = Nothing
+        'txtMultDCS.arrValueMember = Nothing
         Gv1.DataSource = Nothing
         Gv1.Rows.Clear()
         Gv1.Columns.Clear()
