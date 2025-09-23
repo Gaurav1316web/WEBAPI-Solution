acbe2b4321d5b69444038ed4fb98da9122e76c83
Gaurav Kumar Singh <tecxpert071@outlook.com>
2025-09-23 18:09:17 +0530

    Outstanding Report For JPR and DCSBankAdvice For ALW

--- XpertERPBulkProcurement/Milk Procurement/Report/frmVendorBankAdvice.Designer.vb
+++ XpertERPBulkProcurement/Milk Procurement/Report/frmVendorBankAdvice.Designer.vb
@@ -184,7 +184,7 @@ Partial Class frmVendorBankAdvice
         Me.RadPageView1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.RadPageView1.Location = New System.Drawing.Point(0, 0)
         Me.RadPageView1.Name = "RadPageView1"
-        Me.RadPageView1.SelectedPage = Me.RadPageViewPage2
+        Me.RadPageView1.SelectedPage = Me.RadPageViewPage1
         Me.RadPageView1.Size = New System.Drawing.Size(815, 344)
         Me.RadPageView1.TabIndex = 11
         CType(Me.RadPageView1.GetChildAt(0), Telerik.WinControls.UI.RadPageViewStripElement).StripButtons = Telerik.WinControls.UI.StripViewButtons.None
