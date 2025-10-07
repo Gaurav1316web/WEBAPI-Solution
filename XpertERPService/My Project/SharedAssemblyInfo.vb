fbe0b0b07142d943a9ec854d28339c77b0204374
balwinder.singh@tecxpert.in <balwinder.singh@tecxpert.in>
2025-10-07 13:08:35 +0530

    Exe version 7.1.5.16

--- XpertERPService/My Project/SharedAssemblyInfo.vb
+++ XpertERPService/My Project/SharedAssemblyInfo.vb
@@ -7,12 +7,12 @@ Imports System.Runtime.InteropServices
 ' associated with an assembly.
 ' Review the values of the assembly attributes 
 <Assembly: AssemblyTitle("XPERT ERP")>
-<Assembly: AssemblyDescription("06-10-2025")>
+<Assembly: AssemblyDescription("07-10-2025")>
 <Assembly: AssemblyCompany("Tecxpert Software Private Limited")>
 <Assembly: AssemblyCopyright("Copyright © Tecxpert 2008-2025")>
 <Assembly: AssemblyTrademark("")>
 <Assembly: ComVisible(True)>
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("30f2e993-87ce-4f88-a9fa-ea12e3f50930")>
-<Assembly: AssemblyVersion("7.1.5.15")>
-<Assembly: AssemblyFileVersion("7.1.5.15")>
+<Assembly: AssemblyVersion("7.1.5.19")>
+<Assembly: AssemblyFileVersion("7.1.5.19")>
