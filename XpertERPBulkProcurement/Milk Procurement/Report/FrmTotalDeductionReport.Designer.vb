4c15c65cceeca95862ea257059b40ca7a90b09fd
Gaurav Kumar Singh <tecxpert071@outlook.com>
2025-11-27 15:56:51 +0530

    Added ItemWiseDetail Report

--- XpertERPBulkProcurement/Milk Procurement/Report/FrmTotalDeductionReport.Designer.vb
+++ XpertERPBulkProcurement/Milk Procurement/Report/FrmTotalDeductionReport.Designer.vb
@@ -23,7 +23,7 @@ Partial Class FrmTotalDeductionReport
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim TableViewDefinition7 As Telerik.WinControls.UI.TableViewDefinition = New Telerik.WinControls.UI.TableViewDefinition()
+        Dim TableViewDefinition1 As Telerik.WinControls.UI.TableViewDefinition = New Telerik.WinControls.UI.TableViewDefinition()
         Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
         Me.RadPageView1 = New Telerik.WinControls.UI.RadPageView()
         Me.RadPageViewPage1 = New Telerik.WinControls.UI.RadPageViewPage()
@@ -503,7 +503,7 @@ Partial Class FrmTotalDeductionReport
         Me.gv1.MasterTemplate.SelectionMode = Telerik.WinControls.UI.GridViewSelectionMode.CellSelect
         Me.gv1.MasterTemplate.ShowGroupedColumns = True
         Me.gv1.MasterTemplate.ShowHeaderCellButtons = True
-        Me.gv1.MasterTemplate.ViewDefinition = TableViewDefinition7
+        Me.gv1.MasterTemplate.ViewDefinition = TableViewDefinition1
         Me.gv1.MyExportFilePath = ""
         Me.gv1.MyStopExport = False
         Me.gv1.Name = "gv1"
