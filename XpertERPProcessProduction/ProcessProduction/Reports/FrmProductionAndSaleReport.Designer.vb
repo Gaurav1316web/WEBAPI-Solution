244c63fb081ebc24c57c2d6de01de546343d2a68
varsha <varsha.kumari@tecxpert.in>
2025-12-30 17:39:37 +0530

    merge

--- XpertERPProcessProduction/ProcessProduction/Reports/FrmProductionAndSaleReport.Designer.vb
+++ XpertERPProcessProduction/ProcessProduction/Reports/FrmProductionAndSaleReport.Designer.vb
@@ -209,7 +209,7 @@ Partial Class FrmProductionAndSaleReport
         Me.RadPageView1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.RadPageView1.Location = New System.Drawing.Point(0, 0)
         Me.RadPageView1.Name = "RadPageView1"
-        Me.RadPageView1.SelectedPage = Me.RadPageViewPage2
+        Me.RadPageView1.SelectedPage = Me.RadPageViewPage1
         Me.RadPageView1.Size = New System.Drawing.Size(1078, 440)
         Me.RadPageView1.TabIndex = 14
         CType(Me.RadPageView1.GetChildAt(0), Telerik.WinControls.UI.RadPageViewStripElement).StripButtons = Telerik.WinControls.UI.StripViewButtons.None
