2f77f69a001892266bdce57dd4316f4ea8c3ee6f
varsha <varsha.kumari@tecxpert.in>
2026-01-02 11:28:30 +0530

    MERGE

---
+++ XpertERPDairySale/Report/rptItemWiseBillReport.vb
@@ -0,0 +1,12 @@
+﻿Imports common
+Imports System.Globalization
+Imports System.Data.SqlClient
+Imports System.Net
+Imports XpertERPEngine
+Imports System.Text.RegularExpressions
+Public Class rptItemWiseBillReport
+    Inherits FrmMainTranScreen
+    Private Sub rptItemWiseBillReport_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
