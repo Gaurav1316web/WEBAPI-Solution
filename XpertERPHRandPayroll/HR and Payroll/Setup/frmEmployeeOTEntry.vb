2df2c7287b79feb6d66545a9d0b8a9a46f0a9482
Gaurav Kumar Singh <tecxpert071@outlook.com>
2026-03-06 10:21:02 +0530

    OTEntry Screen Added

---
+++ XpertERPHRandPayroll/HR and Payroll/Setup/frmEmployeeOTEntry.vb
@@ -0,0 +1,11 @@
+﻿Imports common
+Imports System.Data
+Imports XpertERPEngine
+Imports System.Data.SqlClient
+Imports Telerik.WinControls.UI
+Public Class frmEmployeeOTEntry
+    Inherits FrmMainTranScreen
+    Private Sub frmEmployeeOTEntry_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
